559237bdd9257facca00aca47d18d759cb0ae1e3
Shisato <shisatoyano@gmail.com>
2022-07-15 23:34:47 +0900

    implemented test code for axle.py

---
+++ test/test_axle.py
@@ -0,0 +1,20 @@
+"""
+Test code of Vehicle axle drawing
+
+Author: Shisato Yano
+"""
+
+import pytest
+import sys
+import os
+
+# テストターゲットのモジュールを読み込むためのパス設定
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../vehicle_drawing")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../common")
+import axle
+
+
+def test_main():
+    axle.show_plot = False
+    
+    assert axle.main() == True
