fc2e89382135ae3c85b6f4a7e69ec73dfe61864d
Shisato <shisatoyano@gmail.com>
2022-07-16 16:23:45 +0900

    add test code of compare linear motion model

---
+++ test/test_compare_linear_model.py
@@ -0,0 +1,21 @@
+"""
+Test code of Comparing linear motion model
+
+Author: Shisato Yano
+"""
+
+import pytest
+import sys
+import os
+
+# テストターゲットのモジュールを読み込むためのパス設定
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../vehicle_drawing")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../common")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../motion_model/linear_motion")
+import compare_linear_model as clm
+
+
+def test_simulation():
+    clm.show_plot = False
+    
+    assert clm.main() == True
