fd69dd4ab1c524251c6f9d85b6a483a8562617f4
Shisato <shisatoyano@gmail.com>
2022-07-16 13:13:39 +0900

    add test code of chassis drawing

---
+++ test/test_chassis.py
@@ -0,0 +1,20 @@
+"""
+Test code of Vehicle chassis drawing
+
+Author: Shisato Yano
+"""
+
+import pytest
+import sys
+import os
+
+# テストターゲットのモジュールを読み込むためのパス設定
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../vehicle_drawing")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../common")
+import chassis
+
+
+def test_draw():
+    chassis.show_plot = False
+    
+    assert chassis.main() == True
