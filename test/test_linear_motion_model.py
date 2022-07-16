d774e0105524bce92db640eed9907215cb95d8b6
Shisato <shisatoyano@gmail.com>
2022-07-16 15:10:43 +0900

    add test code of linear motion model simulation

---
+++ test/test_linear_motion_model.py
@@ -0,0 +1,21 @@
+"""
+Test code of Linear motion model
+
+Author: Shisato Yano
+"""
+
+import pytest
+import sys
+import os
+
+# テストターゲットのモジュールを読み込むためのパス設定
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../vehicle_drawing")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../common")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../motion_model/")
+import linear_motion_model.linear_motion_model as lmm
+
+
+def test_simulation():
+    lmm.show_plot = False
+    
+    assert lmm.main() == True
