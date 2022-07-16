796878751ca50ab8471b502a76a1cacb7b7af2f0
Shisato <shisatoyano@gmail.com>
2022-07-16 10:02:08 +0900

    add test code of body drawing

---
+++ test/test_body.py
@@ -0,0 +1,20 @@
+"""
+Test code of Vehicle body drawing
+
+Author: Shisato Yano
+"""
+
+import pytest
+import sys
+import os
+
+# テストターゲットのモジュールを読み込むためのパス設定
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../vehicle_drawing")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../common")
+import body
+
+
+def test_main():
+    body.show_plot = False
+    
+    assert body.main() == True
