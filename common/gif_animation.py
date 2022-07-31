9cb0dac23ec2c39afe436532546e68c91649f86f
Shisato <shisatoyano@gmail.com>
2022-07-31 13:53:41 +0900

    implemented gif creation method

--- common/gif_animation.py
+++ common/gif_animation.py
@@ -4,6 +4,11 @@ GIF animation creating program
 Author: Shisato Yano
 """
 
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
 
 class GifAnimation:
     """
@@ -13,6 +18,37 @@ class GifAnimation:
 
     def __init__(self, save_name_path, duration_ms):
         self.image_counter = 0
-        self.image_array = []
+        self.images_array = []
         self.save_name_path = save_name_path
         self.duration_ms = duration_ms
+    
+    def save_image(self):
+        plt.savefig(str(self.image_counter) + ".png")
+        self.image_counter += 1
+    
+    def append_image(self):
+        for num in range(self.image_counter):
+            image = Image.open(str(num) + ".png")
+            self.images_array.append(image)
+    
+    def remove_image(self):
+        for num in range(self.image_counter):
+            os.remove(str(num) + ".png")
+    
+    def create_gif(self):
+        if self.image_counter == 0:
+            print("Can not create gif because there is no image file..")
+            return False
+        else:
+            self.append_image()
+            
+            print("Created " + self.save_name_path)
+            self.images_array[0].save(self.save_name_path, 
+                                      save_all=True, 
+                                      append_images=self.images_array[1:],
+                                      loop=0,
+                                      duration=self.duration_ms)
+            
+            self.remove_image()
+            
+            return True
