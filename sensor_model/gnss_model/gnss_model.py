dca39ea9a429920beebf43001cabed79c3de8e5e
Shisato <shisatoyano@gmail.com>
2022-08-23 23:24:46 +0900

    implemented init

--- sensor_model/gnss_model/gnss_model.py
+++ sensor_model/gnss_model/gnss_model.py
@@ -8,10 +8,18 @@ Author: Shisato Yano
 class GnssModel:
     """
     GNSSによる測位を模擬するクラス
+    正規分布に従う誤差を含んだx, y座標を出力する
     """
 
     def __init__(self, x_noise_std=0.5, y_noise_std=0.5, interval_sec=0.5):
         """
         コンストラクタ
-        
+        x_noise_std: x座標に含まれる誤差の標準偏差[m]
+        y_noise_std: y座標に含まれる誤差の標準偏差[m]
+        interval_sec: 測位される周期[sec]
         """
+
+        # パラメータのセット
+        self.x_noise_std = x_noise_std
+        self.y_noise_std = y_noise_std
+        self.interval_sec = interval_sec
