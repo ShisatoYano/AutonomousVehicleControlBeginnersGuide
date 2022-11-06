7aa6c213130c49424163ee0308ea4f1397435034
Shisato <shisatoyano@gmail.com>
2022-11-06 17:13:15 +0900

    defined output from motion model as vector

--- Modeling/Sources/motion/compare_linear_model.py
+++ Modeling/Sources/motion/compare_linear_model.py
@@ -7,8 +7,9 @@ Author: Shisato Yano
 import sys
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
-# 他のディレクトリにあるモジュールを読み込むためのパス設定
+# path setting to import external module
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../vehicle")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../Basis/Sources/transformation")
 from four_wheels_vehicle import FourWheelsVehicle
@@ -16,24 +17,47 @@ from transformation import convert_speed_kmh_2_ms
 from linear_motion_model import LinearMotionModel
 from accurate_linear_motion_model import AccurateLinearMotionModel
 
-# パラメータ定数
+# parameters constant
 INTERVAL_SEC = 0.1
 INTERVAL_MSEC = INTERVAL_SEC * 1000
 TIME_LIMIT_SEC = 30
 SPEED_KMH = 20
 YAW_RATE_DS = 15
 
-# グラフの出力有無を切り替えるフラグ
+# flag to switch showing plot
+# when this module is called for unit testing,
+# this flag is set as false not to show plot
 show_plot = True
 
 
+def calculate_input(u, t):
+    """
+    Function to calculate input [speed[m/sex], yaw rate[rad/sec]]
+
+    u: input vector [speed[m/sex], yaw rate[rad/sec]]
+    t: elapsed time[sec]
+    """
+
+    if t <= TIME_LIMIT_SEC/2:
+        u[1, 0] = np.deg2rad(YAW_RATE_DS)
+    else:
+        u[1, 0] = -np.deg2rad(YAW_RATE_DS)
+    u[0, 0] = convert_speed_kmh_2_ms(SPEED_KMH)
+
+    return u
+
+
 def main():
+    """
+    Main function
+    """
+
     print(__file__ + " + start!!")
 
-    # 既に作成されている図があればクリア
+    # clear all figures
     plt.clf()
 
-    # 描画の設定
+    # initialize figure
     ax = plt.subplot(1, 1, 1)
     ax.set_xlabel("X[m]")
     ax.set_ylabel("Y[m]")
@@ -41,54 +65,52 @@ def main():
     ax.set_title("Blue:Normal Red:Accurate")
     ax.grid(True)
 
-    # 通常の直線運動モデル
-    lmm = LinearMotionModel(interval_sec=INTERVAL_SEC) # モデル
-    lmm_fwv = FourWheelsVehicle(ax, color='b') # 描画オブジェクト
-    x_lmm, y_lmm, yaw_lmm, steer_lmm = 0.0, 0.0, 0.0, 0.0 # 計算される位置、方位角、ステア角
-    x_lmm_all, y_lmm_all = [], [] # 計算されたx, y座標を記録する配列
+    # generate instance
+    # motion model
+    lmm = LinearMotionModel(interval_sec=INTERVAL_SEC) # normal
+    almm = AccurateLinearMotionModel(interval_sec=INTERVAL_SEC) # accurate
+    # vehicle drawing
+    lmm_fwv = FourWheelsVehicle(ax, color='b') # normal model is colored blue
+    almm_fwv = FourWheelsVehicle(ax, color='r') # accurate model is colored red
+    
+    # initialize data
+    elapsed_time_sec = 0.0
+    state_normal, state_accurate = np.zeros((3, 1)), np.zeros((3, 1))
+    input_vector = np.zeros((2, 1))
+    x_lmm_all, y_lmm_all = [], [] # position data list for normal
+    x_almm_all, y_almm_all = [], [] # position data list for accurate
 
-    # 高精度版の直線運動モデル
-    almm = AccurateLinearMotionModel(interval_sec=INTERVAL_SEC) # モデル
-    almm_fwv = FourWheelsVehicle(ax, color='r') # 描画オブジェクト
-    x_almm, y_almm, yaw_almm, steer_almm = 0.0, 0.0, 0.0, 0.0 # 計算される位置、方位角、ステア角
-    x_almm_all, y_almm_all = [], [] # 計算されたx, y座標を記録する配列
-
-    elapsed_time_sec = 0.0 # 経過時間[s]
-    speed_input, yaw_rate_input = 0.0, 0.0 # 入力する速度[m/s], 角速度[deg/s]
-
-    # シミュレーション実行
+    # simulation loop
     while TIME_LIMIT_SEC >= elapsed_time_sec:
-        # 入力値の決定
-        if elapsed_time_sec <= TIME_LIMIT_SEC/2:
-            yaw_rate_input = YAW_RATE_DS
-        else:
-            yaw_rate_input = -YAW_RATE_DS
-        speed_input = convert_speed_kmh_2_ms(SPEED_KMH)
+        # calculate input, speed and yaw rate
+        input_vector = calculate_input(u=input_vector, t=elapsed_time_sec)
 
-        # 通常の運動モデルによる計算
-        x_lmm, y_lmm, yaw_lmm, steer_lmm = lmm.calculate_state(x_lmm, y_lmm, yaw_lmm, speed_input, yaw_rate_input)
-        x_lmm_all.append(x_lmm), y_lmm_all.append(y_lmm)
-        # 車両を描画
-        lmm_fwv.draw(x_lmm, y_lmm, yaw_lmm, steer_lmm)
+        # calculate state by normal motion model
+        state_normal, steer_lmm_rad = lmm.calculate_state(x=state_normal, u=input_vector)
+        x_lmm_all.append(state_normal[0, 0]), y_lmm_all.append(state_normal[1, 0]) # record data
+        
+        # calculate state by accurate motion model
+        state_accurate, steer_almm_rad = almm.calculate_state(x=state_accurate, u=input_vector)
+        x_almm_all.append(state_accurate[0, 0]), y_almm_all.append(state_accurate[1, 0])
 
-        # 高精度版の運動モデルによる計算
-        x_almm, y_almm, yaw_almm, steer_almm = almm.calculate_state(x_almm, y_almm, yaw_almm, speed_input, yaw_rate_input)
-        x_almm_all.append(x_almm), y_almm_all.append(y_almm)
-        # 車両を描画
-        almm_fwv.draw(x_almm, y_almm, yaw_almm, steer_almm)
+        # draw vehicle by normal model
+        lmm_fwv.draw(state_normal[0, 0], state_normal[1, 0], np.rad2deg(state_normal[2, 0]), np.rad2deg(steer_lmm_rad))
 
-        # 車両の位置に合わせて描画範囲を更新
-        ax.set_xlim([(x_lmm + x_almm)/2 - 5, (x_lmm + x_almm)/2 + 5])
-        ax.set_ylim([(y_lmm + y_almm)/2 - 5, (y_lmm + y_almm)/2 + 5])
+        # draw vehicle by accurate model
+        almm_fwv.draw(state_accurate[0, 0], state_accurate[1, 0], np.rad2deg(state_accurate[2, 0]), np.rad2deg(steer_almm_rad))
 
-        # 時間を進める
+        # update plot range
+        ax.set_xlim([(state_normal[0, 0] + state_accurate[0, 0])/2 - 5, (state_normal[0, 0] + state_accurate[0, 0])/2 + 5])
+        ax.set_ylim([(state_normal[1, 0] + state_accurate[1, 0])/2 - 5, (state_normal[1, 0] + state_accurate[1, 0])/2 + 5])
+
+        # increment time
         elapsed_time_sec += INTERVAL_SEC
 
-        # ユニットテスト時はこのフラグをFlaseにする
-        # グラフが表示されるとテストが進まなくなる
-        if show_plot: plt.pause(INTERVAL_SEC) # 一度描画するたびにインターバル時間分だけポーズ
+        # pause temporarily to show animation
+        # only when flag to show plot is true
+        if show_plot: plt.pause(INTERVAL_SEC)
     
-    # それぞれのモデルによる走行軌跡を描画して比較
+    # show calculated trejectory by both models and compare
     plt.plot(x_lmm_all, y_lmm_all, ".b")
     plt.plot(x_almm_all, y_almm_all, ".r")
     plt.xlabel("X[m]")
