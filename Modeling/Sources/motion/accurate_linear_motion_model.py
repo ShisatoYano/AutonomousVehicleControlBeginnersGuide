61bbbbd3e761667ec79c2dff41cf052526c5aaf5
Shisato <shisatoyano@gmail.com>
2022-11-06 16:54:06 +0900

    define accurate motion model as state equation

--- Modeling/Sources/motion/accurate_linear_motion_model.py
+++ Modeling/Sources/motion/accurate_linear_motion_model.py
@@ -1,6 +1,10 @@
 """
 Accurate linear motion model program
 
+x_{t+1} = x_t + v_t * cos(yaw_t + omega_t*dt/2) * dt
+y_{t+1} = y_t + v_t * sin(yaw_t + omega_t*dt/2) * dt
+yaw_{t+1} = yaw_t + omega_t * dt
+
 Author: Shisato Yano
 """
 
@@ -10,125 +14,218 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import sin, cos, asin
 
-# 他のディレクトリにあるモジュールを読み込むためのパス設定
+# path setting to import external module
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../vehicle")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../Basis/Sources/transformation")
 from four_wheels_vehicle import FourWheelsVehicle
 from transformation import convert_speed_kmh_2_ms
 
-# パラメータ定数
+# parameters constant
 INTERVAL_SEC = 0.1
 INTERVAL_MSEC = INTERVAL_SEC * 1000
 TIME_LIMIT_SEC = 30
 SPEED_KMH = 20
 YAW_RATE_DS = 15
 
-# グラフの出力有無を切り替えるフラグ
+# flag to switch showing plot
+# when this module is called for unit testing,
+# this flag is set as false not to show plot
 show_plot = True
 
 
 class AccurateLinearMotionModel:
     """
-    直線運動モデルに従って車両の位置と方位を計算するクラス
-    x, yの移動にて微小時間の真ん中の時刻の方位を使用する
-    モデルによるオフセット誤差を少なくするための工夫
+    Class to predict 3 dimensional state (position x, y and yaw angle)
+    by Accurate Linear Motion Model
+    Yaw angle at center of interval time is used to calculate state
+    Tips to decrease offset error by modeling
     """
 
-    def __init__(self, front_length_m=2.0, rear_length_m=0.0, interval_sec=0.05):
+    def __init__(self, front_length_m=2.0, rear_length_m=0.0, 
+                 interval_sec=0.1, speed_noise_std_mps=0.0,
+                 yaw_rate_noise_std_rps=0.0):
         """
-        コンストラクタ
-        front_length_m: 車両位置から前方への長さ[m]
-        rear_length_m: 車両位置から後方への長さ[m]
-        interval_sec: 移動前後の刻み時間[sec]
+        Constructor
+        front_length_m: length from vehicle origin to front edge[m]
+        rear_length_m: length from vehicle origin to rear edge[m]
+        interval_sec: interval time between previous and current step[sec]
+        speed_noise_std_mps: standard deviation included in speed input[m/sec]
+        yaw_rate_noise_std_rps: standard deviation included in yaw rate input[rad/sec]
         """
 
-        # パラメータのセット
-        self.wheel_base_m = front_length_m + rear_length_m # ホイールベース(前輪と後輪の間の距離)
-        self.interval_sec = interval_sec
+        # setting parameters
+        self.WHEEL_BASE_M = front_length_m + rear_length_m
+        self.DT = interval_sec
+        self.SPEED_NOISE_STD = speed_noise_std_mps
+        self.YAW_RATE_NOISE_STD = yaw_rate_noise_std_rps
 
-        # メンバ変数の初期化
-        self.speed_ms = 0.0
-        self.yaw_rate_ds = 0.0
-        self.turning_radius_m = 0.0
-        self.curvature = 0.0
+        self.matrix_A_in_state_equation()
+        self.input_covariance_matrix()
+
+    def matrix_A_in_state_equation(self):
+        """
+        Matrix A in state equation
+        x_{t+1} = A * x_t + G * u_t
+        """
+
+        self.A = np.array([[1.0, 0.0, 0.0],
+                           [0.0, 1.0, 0.0],
+                           [0.0, 0.0, 1.0]])
     
-    def calculate_state(self, x_m, y_m, yaw_deg, speed_ms, yaw_rate_ds):
+    def matrix_B_in_state_equation(self, yaw_rad, yaw_rate_rps):
         """
-        速度[m/s]と角速度[deg/s]から車両の位置(x, y)と方位(yaw)を計算する関数
-        x_m, y_m, yaw_deg: 1時刻前のx, y座標、方位yaw
-        speed_ms: その時刻での速度[m/s]
-        yaw_rate_ds: その時刻での角速度[deg/s]
+        Matrix B in state equation
+        x_{t+1} = A * x_t + B * u_t
         """
 
-        # 入力
-        self.speed_ms = speed_ms
-        self.yaw_rate_ds = yaw_rate_ds
+        return np.array([[cos(yaw_rad + yaw_rate_rps*self.DT/2) * self.DT, 0.0],
+                         [sin(yaw_rad + yaw_rate_rps*self.DT/2) * self.DT, 0.0],
+                         [0.0, self.DT]])
+    
+    def input_covariance_matrix(self):
+        """
+        System noise covariance matrix (2 x 2)
+        [[speed input std^2 0.0],
+         [0.0, yaw rate input std^2]]
+        """
 
-        # 直線運動モデルに従って位置と方位を計算
-        theta = np.deg2rad(yaw_deg) + (np.deg2rad(yaw_rate_ds)*self.interval_sec/2)
-        x_m_next = x_m + speed_ms * cos(theta) * self.interval_sec
-        y_m_next = y_m + speed_ms * sin(theta) * self.interval_sec
-        yaw_deg_next = yaw_deg + yaw_rate_ds * self.interval_sec
+        self.Q = np.zeros((2, 2))
+        self.Q[0, 0] = self.SPEED_NOISE_STD ** 2
+        self.Q[1, 1] = self.YAW_RATE_NOISE_STD ** 2
+    
+    def jacobian_F(self, speed_mps, yaw_rad, yaw_rate_rps):
+        """
+        Jacobian matrix of Motion model
+        about state [x, y, yaw]
+
+        dx/dx = 1, dx/dy = 0, dx/dyaw = -v*sin(yaw + yaw_rate*dt/2)*dt
+        dy/dx = 0, dy/dy = 1, dy/dyaw = v*cos(yaw + yaw_rate*dt/2)*dt
+        dyaw/dx = 0, dyaw/dy = 0, dyaw/dyaw = 1 
+        """
+
+        return np.array([[1.0, 0.0, -speed_mps*sin(yaw_rad + yaw_rate_rps*self.DT/2)*self.DT],
+                         [0.0, 1.0, speed_mps*cos(yaw_rad + yaw_rate_rps*self.DT/2)*self.DT],
+                         [0.0, 0.0, 1.0]])
+    
+    def jacobian_G(self, speed_mps, yaw_rad, yaw_rate_rps):
+        """
+        Jacobian matrix of Motion model
+        about input [speed, yaw rate]
+        Speed: v
+        Yaw rate: ω
+
+        dx/dv = cos(yaw + yaw_rate*dt/2)*dt, dx/dω = dt/2 * v * -sin(yaw + yaw_rate*dt/2)*dt
+        dy/dv = sin(yaw + yaw_rate*dt/2)*dt, dy/dω = dt/2 * v * cos(yaw + yaw_rate*dt/2)*dt
+        dyaw/dv = 0, dyaw/dω = dt
+        """
+
+        return np.array([[cos(yaw_rad+yaw_rate_rps*self.DT/2)*self.DT, -self.DT*speed_mps*sin(yaw_rad + yaw_rate_rps*self.DT/2)*self.DT/2],
+                         [sin(yaw_rad+yaw_rate_rps*self.DT/2)*self.DT, self.DT*speed_mps*cos(yaw_rad + yaw_rate_rps*self.DT/2)*self.DT/2],
+                         [0.0, self.DT]])
+
+    def calculate_state(self, x, u):
+        """
+        Function of state equation
+        This is used to predict vehicle's position and velocity at next time
+        by using acceleration input
+        x_{t+1} = F * x_t + G * u_t
+
+        x: state vector [x[m], y[m], yaw[rad]]' at previous step
+        u: input vector [speed[m/s], yaw rate[rad/s]]' at current step
+        """
+
+        B = self.matrix_B_in_state_equation(x[2, 0], u[1, 0])
+        x = self.A @ x + B @ u
+
+        # calculate steering angle from speed and yaw rate
+        # zero division error will happen when speed is zero
+        steer_rad = asin(self.WHEEL_BASE_M * u[1, 0] / u[0, 0])
+
+        return x, steer_rad
+    
+    def calculate_covariance(self, x, p):
+        """
+        Function to predict covariance of position and velocity
+        p_{t+1} = F * p^_t * F^T + G * Q * G^T
+
+        x: state vector [x[m], y[m], yaw[rad]]' at previous step
+        p: estimated covariance at previous time, p^_t
+        """
+
+        F = self.jacobian_F(u[0, 0], x[2, 0], u[1, 0])
+        G = self.jacobian_G(u[0, 0], x[2, 0], u[1, 0])
         
-        # 速度と角速度からステアリング角度を計算
-        # 速度が0になると0割りになるので注意
-        steer_rad = asin(self.wheel_base_m * np.deg2rad(yaw_rate_ds)/speed_ms)
-        
-        return x_m_next, y_m_next, yaw_deg_next, np.rad2deg(steer_rad)
+        return F @ p @ F.T + G @ self.Q @ G.T
+
+
+def calculate_input(u, t):
+    """
+    Function to calculate input [speed[m/sex], yaw rate[rad/sec]]
+
+    u: input vector [speed[m/sex], yaw rate[rad/sec]]
+    t: elapsed time[sec]
+    """
+
+    if t <= TIME_LIMIT_SEC/2:
+        u[1, 0] = np.deg2rad(YAW_RATE_DS)
+    else:
+        u[1, 0] = -np.deg2rad(YAW_RATE_DS)
+    u[0, 0] = convert_speed_kmh_2_ms(SPEED_KMH)
+
+    return u
 
 
 def main():
+    """
+    Main function
+    """
+
     print(__file__ + " + start!!")
 
-    # 既に作成されている図があればクリア
+    # clear all figures
     plt.clf()
 
-    # 描画の設定
+    # initialize figure
     ax = plt.subplot(1, 1, 1)
     ax.set_xlabel("X[m]")
     ax.set_ylabel("Y[m]")
     ax.set_aspect("equal")
     ax.grid(True)
 
-    # 直線運動モデルのオブジェクト
-    almm = AccurateLinearMotionModel(interval_sec=INTERVAL_SEC)
+    # generate instances
+    almm = AccurateLinearMotionModel(interval_sec=INTERVAL_SEC) # motion model
+    fwv = FourWheelsVehicle(ax) # vehicle drawing
 
-    # 四輪モデル車両の描画オブジェクト
-    fwv = FourWheelsVehicle(ax)
+    # initialize data
+    elapsed_time_sec = 0.0
+    state_vector = np.zeros((3, 1))
+    input_vector = np.zeros((2, 1))
 
-    elapsed_time_sec = 0.0 # 経過時間[s]
-    speed_input, yaw_rate_input = 0.0, 0.0 # 入力する速度[m/s], 角速度[deg/s]
-    x_m, y_m, yaw_deg, steer_deg = 0.0, 0.0, 0.0, 0.0 # 計算される位置、方位角、ステア角
-
-    # シミュレーション実行
+    # simulation loop
     while TIME_LIMIT_SEC >= elapsed_time_sec:
-        # 入力値の決定
-        if elapsed_time_sec <= TIME_LIMIT_SEC/2:
-            yaw_rate_input = YAW_RATE_DS
-        else:
-            yaw_rate_input = -YAW_RATE_DS
-        speed_input = convert_speed_kmh_2_ms(SPEED_KMH)
+        # calculate input, speed and yaw rate
+        input_vector = calculate_input(u=input_vector, t=elapsed_time_sec)
 
-        # 運動モデルによる計算
-        x_m, y_m, yaw_deg, steer_deg = almm.calculate_state(x_m, y_m, yaw_deg, speed_input, yaw_rate_input)
+        # calculate state by motion model
+        state_vector, steer_rad = almm.calculate_state(x=state_vector, u=input_vector)
 
-        # 計算された位置と方位、ステアに従って車両を描画
-        fwv.draw(x_m, y_m, yaw_deg, steer_deg)
+        # draw vehicle
+        fwv.draw(state_vector[0, 0], state_vector[1, 0], np.rad2deg(state_vector[2, 0]), np.rad2deg(steer_rad))
 
-        # 車両の位置に合わせて描画範囲を更新
-        ax.set_xlim([x_m - 5, x_m + 5])
-        ax.set_ylim([y_m - 5, y_m + 5])
+        # update plot range
+        ax.set_xlim([state_vector[0, 0] - 5, state_vector[0, 0] + 5])
+        ax.set_ylim([state_vector[1, 0] - 5, state_vector[1, 0] + 5])
 
-        # 時間を進める
+        # increment time
         elapsed_time_sec += INTERVAL_SEC
 
-        # ユニットテスト時はこのフラグをFlaseにする
-        # グラフが表示されるとテストが進まなくなる
-        if show_plot: plt.pause(INTERVAL_SEC) # 一度描画するたびにインターバル時間分だけポーズ
+        # pause temporarily to show animation
+        # only when flag to show plot is true
+        if show_plot: plt.pause(INTERVAL_SEC)
 
     return True
 
 
-# メイン関数の実行
+# execute main process
 if __name__ == "__main__":
     main()
