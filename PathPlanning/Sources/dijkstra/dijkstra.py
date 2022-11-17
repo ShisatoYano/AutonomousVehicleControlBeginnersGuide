1417f71fb2325f76cf3c6a1dea817867675f8e8f
Shisato <shisatoyano@gmail.com>
2022-11-17 22:21:39 +0900

    add main function

---
+++ PathPlanning/Sources/dijkstra/dijkstra.py
@@ -0,0 +1,15 @@
+"""
+Path Planning sample program by Dijkstra.
+
+Author: Shisato Yano
+"""
+
+def main():
+    print(__file__ + " start!!")
+
+    return True
+
+
+# execute main process
+if __name__ == "__main__":
+    main()
