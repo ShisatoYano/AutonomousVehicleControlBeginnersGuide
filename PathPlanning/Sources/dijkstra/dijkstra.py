74cee404ac0eefb7fcece1c0c060df65f8228022
Shisato <shisatoyano@gmail.com>
2022-11-17 22:50:42 +0900

    defined parameters

--- PathPlanning/Sources/dijkstra/dijkstra.py
+++ PathPlanning/Sources/dijkstra/dijkstra.py
@@ -4,6 +4,15 @@ Path Planning sample program by Dijkstra.
 Author: Shisato Yano
 """
 
+# parameters
+START_X_M = -5.0 # start point
+START_Y_M = -5.0
+GOAL_X_M = 50.0 # goal point
+GOAL_Y_M = 50.0
+GRID_SIZE_M = 2.0
+VEHICLE_SIZE_HALF_M = 1.5
+
+
 def main():
     print(__file__ + " start!!")
 
