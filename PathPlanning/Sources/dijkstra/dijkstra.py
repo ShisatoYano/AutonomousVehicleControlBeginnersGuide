c5ebcc7727d0cc53b927f3416d8a3da52ebadd5f
Shisato <shisatoyano@gmail.com>
2022-11-18 23:43:17 +0900

    implementing obstacle grid map

--- PathPlanning/Sources/dijkstra/dijkstra.py
+++ PathPlanning/Sources/dijkstra/dijkstra.py
@@ -14,9 +14,18 @@ VEHICLE_SIZE_HALF_M = 1.5
 
 
 class ObstacleGridMap:
-    def __init__(self):
-        self.obst_x_list = []
-        self.obst_y_list = []
+    def __init__(self, start_x_m, start_y_m, goal_x_m, goal_y_m,
+                 grid_size_m, vehicle_size_half_m):
+        self.obst_x = []
+        self.obst_y = []
+    
+    def set_obstacles_positions(self):
+        for i in range(-10, 60): self.obst_x.append(i), self.obst_y.append(-10.0)
+        for i in range(-10, 60): self.obst_x.append(60.0), self.obst_y.append(i)
+        for i in range(-10, 61): self.obst_x.append(i), self.obst_y.append(60.0)
+        for i in range(-10, 60): self.obst_x.append(-10.0), self.obst_y.append(i)
+        for i in range(-10, 40): self.obst_x.append(20.0), self.obst_y.append(i)
+        for i in range(-10, 40): self.obst_x.append(40.0), self.obst_y.append(60.0-i)
 
 
 def main():
