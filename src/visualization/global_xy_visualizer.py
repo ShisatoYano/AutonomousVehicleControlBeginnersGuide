6a5b06e45e8513871a05af9fd03ef4c1e4ecbbdf
Shisato <shisatoyano@gmail.com>
2023-02-20 23:56:46 +0900

    defined draw and main functions

--- src/visualization/global_xy_visualizer.py
+++ src/visualization/global_xy_visualizer.py
@@ -21,3 +21,27 @@ class GlobalXYVisualizer:
         """
 
         self.objects.append(aObject)
+    
+    def draw(self):
+        # setting
+        figure = plt.figure(figsize=(8, 8))
+        axes = figure.add_subplot(111)
+        axes.set_aspect("equal")
+        axes.set_xlim(-5, 5)
+        axes.set_ylim(-5, 5)
+        axes.set_xlabel('X', fontsize=20)
+        axes.set_ylabel('Y', fontsize=20)
+
+        # draw registered objects
+        for obj in self.objects: obj.draw()
+
+        plt.show()
+
+
+def main():
+    vis = GlobalXYVisualizer()
+    vis.draw()
+
+
+if __name__ == "__main__":
+    main()
