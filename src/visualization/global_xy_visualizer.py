f9eefebb3e56c6aee767fe2aaf8c573e955ffb5c
Shisato <shisatoyano@gmail.com>
2023-02-21 22:51:33 +0900

    added prefix a and o

--- src/visualization/global_xy_visualizer.py
+++ src/visualization/global_xy_visualizer.py
@@ -12,28 +12,30 @@ class GlobalXYVisualizer:
     Visualization class for global 2D X-Y plot
     """
     
-    def __init__(self):
-        self.objects = [] # register objects here
+    def __init__(self, a_min=-5, a_max=5):
+        self.o_objects = [] # register objects here
+        self.o_min = a_min
+        self.o_max = a_max
     
-    def add_object(self, aObject):
+    def add_object(self, a_object):
         """
         Method to register object
         """
 
-        self.objects.append(aObject)
+        self.o_objects.append(a_object)
     
     def draw(self):
         # setting
         figure = plt.figure(figsize=(8, 8))
         axes = figure.add_subplot(111)
         axes.set_aspect("equal")
-        axes.set_xlim(-5, 5)
-        axes.set_ylim(-5, 5)
-        axes.set_xlabel('X', fontsize=20)
-        axes.set_ylabel('Y', fontsize=20)
+        axes.set_xlim(self.o_min, self.o_max)
+        axes.set_ylim(self.o_min, self.o_max)
+        axes.set_xlabel('X[m]', fontsize=20)
+        axes.set_ylabel('Y[m]', fontsize=20)
 
         # draw registered objects
-        for obj in self.objects: obj.draw()
+        for obj in self.o_objects: obj.draw()
 
         plt.show()
 
