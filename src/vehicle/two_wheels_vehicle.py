d67a9085d39098912dcf22ff345c576a8a054b8a
Shisato <shisatoyano@gmail.com>
2023-02-24 23:49:05 +0900

    renamed arguments

--- src/vehicle/two_wheels_vehicle.py
+++ src/vehicle/two_wheels_vehicle.py
@@ -17,7 +17,6 @@ class TwoWheelsVehicle:
     Two Wheels Vehicle model class
     """
 
-    def __init__(self, a_pose, front_length_m=2.0, rear_length_m=0.0, 
-                 tire_radius_m=0.3, tire_width_half_m=0.12, color='k', 
-                 line_width=1.0, line_type='-'):
-        self.o_pose = a_pose
+    def __init__(self, pose, f_len_m=2.0, r_len_m=0.0, tire_r_m=0.3,
+                 tire_w_m=0.12, color='k', line_w=1.0, line_type='-'):
+        self.pose = pose
