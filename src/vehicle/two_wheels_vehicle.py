84bf7f5cfb682db4825ffbf3813d10f93f540148
Shisato <shisatoyano@gmail.com>
2023-02-25 22:47:07 +0900

    added spec object as argument

--- src/vehicle/two_wheels_vehicle.py
+++ src/vehicle/two_wheels_vehicle.py
@@ -10,6 +10,7 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../visualization")
 from global_xy_visualizer import GlobalXYVisualizer
+from vehicle_specification import VehicleSpecification
 
 
 class TwoWheelsVehicle:
@@ -17,9 +18,9 @@ class TwoWheelsVehicle:
     Two Wheels Vehicle model class
     """
 
-    def __init__(self, pose, f_len_m=2.0, r_len_m=0.0, tire_r_m=0.3,
-                 tire_w_m=0.12, color='k', line_w=1.0, line_type='-'):
+    def __init__(self, pose, spec):
         self.pose = pose
+        self.spec = spec
     
     def draw(self, axes):
         x_m, y_m, yaw_rad = self.pose
