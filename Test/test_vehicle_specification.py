9903015282ff5869675fa07e6363d3c7ae3dbb66
Shisato <shisatoyano@gmail.com>
2023-02-25 22:31:25 +0900

    implemented and passed unit test

---
+++ Test/test_vehicle_specification.py
@@ -0,0 +1,26 @@
+"""
+Unit test of VehicleSpecification
+
+Author: Shisato Yano
+"""
+
+import pytest
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../src/vehicle")
+from vehicle_specification import VehicleSpecification
+
+
+# test instance
+spec = VehicleSpecification()
+
+
+def test_create_instance():
+    assert spec.f_len_m == 2.0
+    assert spec.r_len_m == 0.0
+    assert spec.tire_r_m == 0.3
+    assert spec.tire_w_m == 0.12
+    assert spec.color == 'k'
+    assert spec.line_w == 1.0
+    assert spec.line_type == '-'
