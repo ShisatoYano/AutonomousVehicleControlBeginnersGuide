e1b7b10170fc1c8b7340905f00c117b5c1ce1575
Shisato <shisatoyano@gmail.com>
2023-02-27 23:16:33 +0900

    implemented constructor and draw method

---
+++ src/vehicle/tire.py
@@ -0,0 +1,38 @@
+"""
+tire.py
+
+Author: Shisato Yano
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Tire:
+    """
+    Vehicle Tire class
+    """
+
+    def __init__(self, spec, x_m, y_m):
+        """
+        Constructor
+        spec: vehicle specification object
+        x_m: offset x[m] on vehicle
+        y_m: offset y[m] on vehicle
+        """
+        
+        self.r_m = spec.tire_r_m
+        self.w_m = spec.tire_w_m
+        self.x_m = x_m
+        self.y_m = y_m
+        self.color = spec.color
+        self.line_w = spec.line_w
+        self.line_type = spec.line_type
+
+        self.points = np.array([
+            [self.r_m, -self.r_m, -self.r_m, self.r_m, self.r_m],
+            [self.w_m, self.w_m, -self.w_m, -self.w_m, self.w_m]
+        ])
+    
+    def draw(self, axes):
+        axes.plot(self.points[0, :], self.points[1, :], lw=self.line_w, color=self.color, ls=self.line_type)
