e721a79e086b43588dd656d9fa3d1f61e45e1d66
Shisato <shisatoyano@gmail.com>
2023-02-28 23:44:45 +0900

    implementing one step animation

--- src/visualization/global_xy_visualizer.py
+++ src/visualization/global_xy_visualizer.py
@@ -5,6 +5,7 @@ Author: Shisato Yano
 """
 
 import matplotlib.pyplot as plt
+import matplotlib.animation as anm
 
 
 class GlobalXYVisualizer:
@@ -28,6 +29,10 @@ class GlobalXYVisualizer:
     def not_show_plot(self):
         self.show_plot = False
 
+    def one_step(self, i, elems, axes):
+        while elems: elems.pop().remove()
+        elems.append(axes.text(-4.4, 4.5, "t = "+str(i)+"[s]", fontsize=15))
+
     def draw(self):
         # setting
         figure = plt.figure(figsize=(8, 8))
@@ -38,10 +43,19 @@ class GlobalXYVisualizer:
         axes.set_xlabel('X[m]', fontsize=20)
         axes.set_ylabel('Y[m]', fontsize=20)
 
-        # draw registered objects
-        for obj in self.objects: obj.draw(axes)
+        elems = []
 
-        if self.show_plot: plt.show()
+        if self.show_plot:
+            self.anime = anm.FuncAnimation(figure, self.one_step, fargs=(elems, axes),
+                                           frames=10, interval=1000, repeat=False)
+            plt.show()
+        else:
+            for i in range(1000): self.one_step(i, elems, axes)
+
+        # draw registered objects
+        # for obj in self.objects: obj.draw(axes)
+
+        # if self.show_plot: plt.show()
 
 
 def main():
