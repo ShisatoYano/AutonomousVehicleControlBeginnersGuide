fd674d136c085efde963848f3351ad33ff2c6760
Shisato <shisatoyano@gmail.com>
2023-03-04 22:38:57 +0900

    implemented test and passed

---
+++ Test/test_agent.py
@@ -0,0 +1,26 @@
+"""
+Unit test of Agent
+
+Author: Shisato Yano
+"""
+
+import pytest
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../src/agent")
+from agent import Agent
+
+
+# test instance
+agent = Agent(1.0, 2.0)
+
+
+def test_create_instance():
+    assert agent.speed_mps == 1.0
+    assert agent.yaw_rate_rps == 2.0
+
+
+def test_control_input():
+    assert agent.control_input()[0] == 1.0
+    assert agent.control_input()[1] == 2.0
