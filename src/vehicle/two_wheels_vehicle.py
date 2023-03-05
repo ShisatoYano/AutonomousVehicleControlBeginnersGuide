579ce8ae2db968eca9ebfc6733d27d78ebdfafe1
Shisato <shisatoyano@gmail.com>
2023-03-05 23:43:53 +0900

    plot state transition animation

--- src/vehicle/two_wheels_vehicle.py
+++ src/vehicle/two_wheels_vehicle.py
@@ -11,12 +11,14 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../visualization")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../agent")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../motion_model")
 from global_xy_visualizer import GlobalXYVisualizer
 from vehicle_specification import VehicleSpecification
 from body import Body
 from chassis import Chassis
 from tire import Tire
 from agent import Agent
+from motion_model import MotionModel
 
 
 class TwoWheelsVehicle:
@@ -24,12 +26,13 @@ class TwoWheelsVehicle:
     Two Wheels Vehicle model class
     """
 
-    def __init__(self, pose, spec, agent=None):
+    def __init__(self, pose, spec, agent=None, motion=None):
         """
         Constructor
         pose: vehicle's pose [x[m], y[m], yaw[rad]]
         spec: vehicle's specification object
         agent: agent to decide control input
+        motion: motion model for state transition
         """
         
         self.pose = pose
@@ -39,11 +42,13 @@ class TwoWheelsVehicle:
         self.front_tire = Tire(spec, spec.f_len_m, 0.0)
         self.rear_tire = Tire(spec, -spec.r_len_m, 0.0)
         self.agent = agent
+        self.motion = motion
         self.poses = [pose]
 
-    def one_step(self, time_s):
-        if not self.agent: return
-        speed_msp, yaw_rate_rps = self.agent.control_input()
+    def one_step(self, time_interval_s):
+        if not self.agent or not self.motion: return
+        order = self.agent.control_order()
+        self.pose = self.motion.state_transition(self.pose, order, time_interval_s)
     
     def draw(self, axes, elems):
         x_m, y_m, yaw_rad = self.pose
@@ -54,14 +59,17 @@ class TwoWheelsVehicle:
         elems += self.rear_tire.draw_object(axes)
 
         self.poses.append(self.pose)
-        elems += axes.plot([p[0] for p in self.poses], [p[1] for p in self.poses], linewidth=self.spec.line_w, color=self.spec.color)
+        elems += axes.plot([p[0, 0] for p in self.poses], [p[1, 0] for p in self.poses], linewidth=0, marker=".", color=self.spec.color)
 
 
 def main():
-    vis = GlobalXYVisualizer()
+    vis = GlobalXYVisualizer(min_lim=0, max_lim=30)
 
     spec = VehicleSpecification()
-    vehicle = TwoWheelsVehicle(np.array([[0.0], [0.0], [0.0]]), spec)
+    agent = Agent(3.0, 0.0)
+    motion = MotionModel(spec)
+    vehicle = TwoWheelsVehicle(np.array([[0.0], [15.0], [0.0]]),
+                               spec, agent, motion)
     
     vis.add_object(vehicle)
     vis.draw()
