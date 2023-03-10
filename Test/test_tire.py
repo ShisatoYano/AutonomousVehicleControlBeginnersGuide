22e5f2d55f6c972860b13e5baf92e432fe497581
Shisato <shisatoyano@gmail.com>
2023-03-11 00:01:45 +0900

    fixed test points fail

--- Test/test_tire.py
+++ Test/test_tire.py
@@ -43,17 +43,17 @@ def test_points():
     assert tire.points.shape[0] == 2
     assert tire.points.shape[1] == 5
 
-    assert tire.points[0, 0] == 2.0
-    assert tire.points[0, 1] == 0.0
-    assert tire.points[0, 2] == 0.0
-    assert tire.points[0, 3] == 2.0
-    assert tire.points[0, 4] == 2.0
+    assert tire.points[0, 0] == 1.0
+    assert tire.points[0, 1] == -1.0
+    assert tire.points[0, 2] == -1.0
+    assert tire.points[0, 3] == 1.0
+    assert tire.points[0, 4] == 1.0
 
-    assert tire.points[1, 0] == 2.0
-    assert tire.points[1, 1] == 2.0
-    assert tire.points[1, 2] == 0.0
-    assert tire.points[1, 3] == 0.0
-    assert tire.points[1, 4] == 2.0
+    assert tire.points[1, 0] == 1.0
+    assert tire.points[1, 1] == 1.0
+    assert tire.points[1, 2] == -1.0
+    assert tire.points[1, 3] == -1.0
+    assert tire.points[1, 4] == 1.0
 
 
 def test_draw():
