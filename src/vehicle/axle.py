a38d830724bb7ea3f5d33fe3ac1cf1f124cef6ed
Shisato <shisatoyano@gmail.com>
2023-03-11 23:03:00 +0900

    defined constructor

--- src/vehicle/axle.py
+++ src/vehicle/axle.py
@@ -3,3 +3,20 @@ axle.py
 
 Author: Shisato Yano
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../common")
+from transformation import Transformation
+
+
+class Axle:
+    """
+    Vehicle Axle class
+    """
+
+    def __init__(self):
+        pass
