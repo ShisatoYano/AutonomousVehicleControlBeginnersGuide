223cdf5d949565d1a7f822b91408469341730340
Shisato <shisatoyano@gmail.com>
2023-03-13 22:57:27 +0900

    implemented initialize, one step and draw tests

--- Test/test_four_wheels_vehicle.py
+++ Test/test_four_wheels_vehicle.py
@@ -28,9 +28,30 @@ class MockSpecification:
         self.line_type = '-'
 
 
+class MockAgent:
+    def __init__(self):
+        self.speed_mps =  0.0
+        self.yaw_rate_rps = 0.0
+
+    def control_order(self):
+        return np.array([[self.speed_mps], [self.yaw_rate_rps]])
+
+
+class MockMotionModel:
+    def __init__(self):
+        pass
+    
+    def state_transition(self, pose, order, time_interval_s):
+        return np.array([[0.0], [0.0], [0.0]])
+    
+    def steering_angle_rad(self, order):
+        return 0.0
+
+
 # test instance
 spec = MockSpecification()
-vehicle = FourWheelsVehicle(np.array([[0.0], [0.0], [0.0]]), spec)
+pose = np.array([[0.0], [0.0], [0.0]])
+vehicle = FourWheelsVehicle(pose, spec)
 
 
 def test_attributes():
@@ -48,3 +69,41 @@ def test_attributes():
     assert hasattr(vehicle, "agent") == True
     assert hasattr(vehicle, "motion") == True
     assert hasattr(vehicle, "poses") == True
+
+
+def test_initialize():
+    assert vehicle.pose[0, 0] == pose[0, 0]
+    assert vehicle.pose[1, 0] == pose[1, 0]
+    assert vehicle.pose[2, 0] == pose[2, 0]
+    assert vehicle.steer_rad == 0.0
+    assert vehicle.spec != None
+    assert vehicle.body != None
+    assert vehicle.chassis != None
+    assert vehicle.front_left_tire != None
+    assert vehicle.front_right_tire != None
+    assert vehicle.rear_left_tire != None
+    assert vehicle.rear_right_tire != None
+    assert vehicle.front_axle != None
+    assert vehicle.rear_axle != None
+    assert vehicle.agent == None
+    assert vehicle.motion == None
+    assert len(vehicle.poses) == 1
+
+
+def test_set_agent_motion():
+    agent = MockAgent()
+    motion = MockMotionModel()
+    vehicle = FourWheelsVehicle(pose, spec, agent=agent, motion=motion)
+
+    assert vehicle.agent != None
+    assert vehicle.motion != None
+
+
+def test_one_step():
+    vehicle.one_step(1.0)
+
+
+def test_draw():
+    figure = plt.figure(figsize=(8, 8))
+    axes = figure.add_subplot(111)
+    vehicle.draw(axes, [])
