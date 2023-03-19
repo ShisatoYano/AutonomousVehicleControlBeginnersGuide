4e13b61ad5a1648615ef86b10555c15cde05a6fc
Shisato <shisatoyano@gmail.com>
2023-03-20 00:03:24 +0900

    implemented rotation method

--- src/array/xy_array.py
+++ src/array/xy_array.py
@@ -4,6 +4,9 @@ xy_array.py
 Author: Shisato Yano
 """
 
+from math import sin, cos
+import numpy as np
+
 
 class XYArray:
     """
@@ -17,3 +20,14 @@ class XYArray:
         """
 
         self.array = array
+    
+    def rotation(self, angle_rad):
+        angle_cos = cos(angle_rad)
+        angle_sin = sin(angle_rad)
+
+        rotation_matrix = np.array([[angle_cos, -angle_sin],
+                                    [angle_sin, angle_cos]])
+        
+        rotated_array = rotation_matrix.dot(self.array)
+
+        return XYArray(rotated_array)
