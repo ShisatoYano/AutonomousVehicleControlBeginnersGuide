da19f90476986813b5ff0aeee84eb19f66d83fd7
Shisato <shisatoyano@gmail.com>
2023-03-20 16:59:19 +0900

    removed unused fields, imports and method

--- src/vehicle/body.py
+++ src/vehicle/body.py
@@ -4,14 +4,6 @@ body.py
 Author: Shisato Yano
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-import os
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../common")
-from transformation import Transformation
-
 
 class Body:
     """
@@ -25,25 +17,6 @@ class Body:
         """
 
         self.array = array
-        
-        # self.f_len_m = spec.f_len_m
-        # self.r_len_m = spec.r_len_m
-        # self.tread_m = spec.tread_m
-        # self.f_edge_m = spec.f_edge_m
-        # self.r_edge_m = spec.r_edge_m
-        # self.width_m = spec.width_m
-        # self.color = spec.color
-        # self.line_w = spec.line_w
-        # self.line_type = spec.line_type
-
-        # self.points = np.array([
-        #     [self.f_edge_m, -self.r_edge_m, -self.r_edge_m, self.f_edge_m, self.f_edge_m],
-        #     [self.width_m, self.width_m, -self.width_m, -self.width_m, self.width_m]
-        # ])
-    
-    def update(self, pose):
-        updated_array = self.array.homogeneous_transformation(pose[0, 0], pose[1, 0], pose[2, 0])
-        return Body(updated_array)
 
     def draw(self, axes, spec, pose):
         transformed_array = self.array.homogeneous_transformation(pose[0, 0], pose[1, 0], pose[2, 0])
