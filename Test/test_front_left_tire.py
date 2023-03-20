ae0861f5ef869b76eedff4cbbc3f8dc22e617f92
Shisato <shisatoyano@gmail.com>
2023-03-20 23:44:46 +0900

    implemented front left tire test

---
+++ Test/test_front_left_tire.py
@@ -0,0 +1,46 @@
+"""
+Unit test of FrontLeftTire
+
+Author: Shisato Yano
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../src/vehicle")
+from front_left_tire import FrontLeftTire
+
+
+# mock class
+class MockSpecification:
+    def __init__(self):
+        self.f_len_m = 1.0
+        self.axle_half_m = 1.0
+        self.tire_r_m = 1.0
+        self.tire_w_m = 1.0
+        self.color = 'k'
+        self.line_w = 1.0
+        self.line_type = '-'
+
+
+# test instance
+spec = MockSpecification()
+tire = FrontLeftTire(spec)
+
+
+def test_create_instance():
+    assert hasattr(tire, "spec") == True
+    assert hasattr(tire, "array") == True
+    assert tire.spec != None
+    assert tire.array != None
+    assert tire.offset_x_m == 1.0
+    assert tire.offset_y_m == 1.0
+
+
+def test_draw():
+    figure = plt.figure(figsize=(8, 8))
+    axes = figure.add_subplot(111)
+    assert tire.draw(axes, np.array([[0.0], [0.0], [0.0]]), 0.0) != None
