ed669a7042eea43f38ab3c1ef0fbbb946f685138
Shisato <shisatoyano@gmail.com>
2023-03-20 15:31:03 +0900

    replaced with fields in spec object

--- src/vehicle/body.py
+++ src/vehicle/body.py
@@ -24,12 +24,12 @@ class Body:
         spec: vehicle specification object
         """
         
-        self.f_len_m = spec.f_len_m - 0.25
-        self.r_len_m = spec.r_len_m - 0.25
-        self.tread_m = 0.25 * (1.0 + spec.f_len_m + spec.r_len_m)
-        self.f_edge_m = self.f_len_m + 0.75
-        self.r_edge_m = self.r_len_m + 0.75
-        self.width_m = 1.0 * self.tread_m
+        self.f_len_m = spec.f_len_m
+        self.r_len_m = spec.r_len_m
+        self.tread_m = spec.tread_m
+        self.f_edge_m = spec.f_edge_m
+        self.r_edge_m = spec.r_edge_m
+        self.width_m = spec.width_m
         self.color = spec.color
         self.line_w = spec.line_w
         self.line_type = spec.line_type
