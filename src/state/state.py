94d61dc1cab9d4b1b8872e5b2104ecf732ccded4
Shisato <shisatoyano@gmail.com>
2023-03-25 23:08:55 +0900

    implemented update method

--- src/state/state.py
+++ src/state/state.py
@@ -4,6 +4,8 @@ state.py
 Author: Shisato Yano
 """
 
+from math import cos, sin, tan
+
 
 class State:
     """
@@ -23,3 +25,21 @@ class State:
         self.y_m = y_m
         self.yaw_rad = yaw_rad
         self.speed_mps = speed_mps
+    
+    def update(self, accel_mps2, steer_rad, time_s, wheel_base_m):
+        """
+        Function to update state
+        accel_mps2: Acceleration[m/s^2]
+        steer_rad: Steering angle[rad]
+        time_s: Time interval per cycle[sec]
+        wheel_base_m: Wheel base's length[m]
+        """
+        updated_x_m = self.x_m + self.speed_mps * cos(self.yaw_rad) * time_s
+        updated_y_m = self.y_m + self.speed_mps * sin(self.yaw_rad) * time_s
+
+        yaw_rate_rad = self.speed_mps * tan(steer_rad) / wheel_base_m
+        updated_yaw_rad = self.yaw_rad + yaw_rate_rad * time_s
+
+        updated_speed_mps = self.speed_mps + accel_mps2 * time_s
+
+        return State(updated_x_m, updated_y_m, updated_yaw_rad, updated_speed_mps)
