d7cf0c7916f05abdadfafdd5901a397c12cf4580
Shisato <shisatoyano@gmail.com>
2023-04-24 07:39:16 +0900

    added test case for save gif name

--- Test/test_global_xy_visualizer.py
+++ Test/test_global_xy_visualizer.py
@@ -13,10 +13,6 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../src/visualizat
 from global_xy_visualizer import GlobalXYVisualizer
 
 
-# test instance
-vis = GlobalXYVisualizer()
-
-
 # mock class
 class MockObject:
     def __init__(self):
@@ -29,7 +25,8 @@ class MockObject:
         pass
 
 
-def test_create_instance():
+def test_initialize():
+    vis = GlobalXYVisualizer()
     assert len(vis.objects) == 0
     assert vis.x_min == 0
     assert vis.x_max == 30
@@ -37,11 +34,30 @@ def test_create_instance():
     assert vis.y_max == 15
     assert vis.time_span_s == 10
     assert vis.time_interval_s == 0.1
+    assert vis.save_gif_name == None
+    assert vis.show_plot == True
+
+
+def test_set_parameters():
+    test_gif_name = "test.gif"
+
+    vis = GlobalXYVisualizer(x_min=-10, x_max=10, y_min=-10, y_max=10,
+                             time_span_s=20, time_interval_s=0.05,
+                             save_gif_name=test_gif_name)
+    assert vis.x_min == -10
+    assert vis.x_max == 10
+    assert vis.y_min == -10
+    assert vis.y_max == 10
+    assert vis.time_span_s == 20
+    assert vis.time_interval_s == 0.05
+    assert vis.save_gif_name == test_gif_name
     assert vis.show_plot == True
 
 
 def test_draw():
     mock = MockObject()
+    vis = GlobalXYVisualizer()
+
     vis.add_object(mock)
     assert len(vis.objects) == 1
 
