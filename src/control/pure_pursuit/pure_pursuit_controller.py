9d218044747491e20823158341697abe7994488f
Shisato <shisatoyano@gmail.com>
2023-04-25 10:22:07 +0900

    changed to call sin curve course as external class

--- src/control/pure_pursuit/pure_pursuit_controller.py
+++ src/control/pure_pursuit/pure_pursuit_controller.py
@@ -12,59 +12,13 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../visualization")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../state")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../vehicle")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../course/sin_curve_course")
 from global_xy_visualizer import GlobalXYVisualizer
 from vehicle_specification import VehicleSpecification
 from state import State
 from state_history import StateHistory
 from four_wheels_vehicle import FourWheelsVehicle
-
-
-class SinCurveCourse:
-    def __init__(self, x_min, x_max, resolution, target_speed_kmph):
-        self.x_array = np.arange(x_min, x_max, resolution)
-        self.y_array = [sin(x / 5.0) * (x / 2.0) for x in self.x_array]
-        self.speed_array = [(target_speed_kmph / 3.6) for _ in self.x_array]
-        self.speed_array[-1] = 0.0
-    
-    def search_nearest_point_index(self, state):
-        vehicle_pos_x_m = state.get_x_m()
-        vehicle_pos_y_m = state.get_y_m()
-
-        diff_x_array = [vehicle_pos_x_m - point_x_m for point_x_m in self.x_array]
-        diff_y_array = [vehicle_pos_y_m - point_y_m for point_y_m in self.y_array]
-        diff_array = np.hypot(diff_x_array, diff_y_array)
-
-        nearest_index = np.argmin(diff_array)
-        
-        return nearest_index
-    
-    def calculate_distance_from_point(self, state, point_index):
-        diff_x_m = state.get_x_m() - self.x_array[point_index]
-        diff_y_m = state.get_y_m() - self.y_array[point_index]
-        return np.hypot(diff_x_m, diff_y_m)
-    
-    def calculate_speed_difference_mps(self, state, point_index):
-        return self.speed_array[point_index] - state.get_speed_mps()
-    
-    def calculate_angle_difference_rad(self, state, point_index):
-        diff_x_m = self.x_array[point_index] - state.get_x_m()
-        diff_y_m = self.y_array[point_index] - state.get_y_m()
-        return atan2(diff_y_m, diff_x_m) - state.get_yaw_rad()
-    
-    def point_x_m(self, point_index):
-        return self.x_array[point_index]
-    
-    def point_y_m(self, point_index):
-        return self.y_array[point_index]
-    
-    def target_speed_mps(self, point_index):
-        return self.speed_array[point_index]
-    
-    def length(self):
-        return len(self.x_array)
-
-    def draw(self, axes, elems):
-        elems += axes.plot(self.x_array, self.y_array, linewidth=0, marker='.', color='r', label="Course")
+from sin_curve_course import SinCurveCourse
 
 
 class PurePursuitController:
