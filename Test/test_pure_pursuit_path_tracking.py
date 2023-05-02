77ed58af203eedceeb20e060056f9db72e99f812
Shisato <shisatoyano@gmail.com>
2023-05-03 00:25:20 +0900

    passed pure pursuit simulation test

---
+++ Test/test_pure_pursuit_path_tracking.py
@@ -0,0 +1,18 @@
+"""
+Test of Path tracking simulation by Pure pursuit algorithm
+
+Author: Shisato Yano
+"""
+
+from pathlib import Path
+import sys
+import pytest
+
+sys.path.append(str(Path(__file__).absolute().parent) + "/../src/simulation/pure_pursuit_path_tracking")
+import pure_pursuit_path_tracking
+
+
+def test_simulation():
+    pure_pursuit_path_tracking.show_plot = False
+
+    pure_pursuit_path_tracking.main()
