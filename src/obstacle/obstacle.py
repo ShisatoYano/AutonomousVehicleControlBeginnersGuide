e51c437f3939069e48a47899796b48f20fa11e03
Shisato <shisatoyano@gmail.com>
2023-05-05 00:07:49 +0900

    implemented constructor

---
+++ src/obstacle/obstacle.py
@@ -0,0 +1,21 @@
+"""
+obstacle.py
+
+Author: Shisato Yano
+"""
+
+import numpy as np
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).absolute().parent) + "/../array")
+from xy_array import XYArray
+
+
+class Obstacle:
+    def __init__(self, state, length_m=2.0, width_m=2.0):
+        self.state = state
+        
+        contour = np.array([[length_m, -length_m, -length_m, length_m, length_m],
+                            [width_m, width_m, -width_m, -width_m, width_m]])
+        self.array = XYArray(contour)
