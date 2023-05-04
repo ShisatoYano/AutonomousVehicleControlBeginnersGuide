29742cfc62d84d3da66087bdefaaf88669dd8848
Shisato <shisatoyano@gmail.com>
2023-05-04 21:30:03 +0900

    append rear right tire plot into elems array

--- src/vehicle/rear_right_tire.py
+++ src/vehicle/rear_right_tire.py
@@ -35,4 +35,5 @@ class RearRightTire:
         translated_array = self.array.homogeneous_transformation(self.offset_x_m, self.offset_y_m, 0.0)
         transformed_array = translated_array.homogeneous_transformation(pose[0, 0], pose[1, 0], pose[2, 0])
         data = transformed_array.get_data()
-        elems += axes.plot(data[0, :], data[1, :], lw=self.spec.line_w, color=self.spec.color, ls=self.spec.line_type)
+        tire_plot, = axes.plot(data[0, :], data[1, :], lw=self.spec.line_w, color=self.spec.color, ls=self.spec.line_type)
+        elems.append(tire_plot)
