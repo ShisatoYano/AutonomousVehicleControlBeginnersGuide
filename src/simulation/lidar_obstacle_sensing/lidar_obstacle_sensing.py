d7ec3ba4679d55dcac06cc0a15b715412b36e4f6
Shisato <shisatoyano@gmail.com>
2023-05-05 23:17:59 +0900

    create obst list instance

--- src/simulation/lidar_obstacle_sensing/lidar_obstacle_sensing.py
+++ src/simulation/lidar_obstacle_sensing/lidar_obstacle_sensing.py
@@ -20,6 +20,7 @@ from state import State
 from state_history import StateHistory
 from four_wheels_vehicle import FourWheelsVehicle
 from obstacle import Obstacle
+from obstacle_list import ObstacleList
 
 
 show_plot = True
@@ -28,17 +29,15 @@ show_plot = True
 def main():
     vis = GlobalXYVisualizer(x_min=-30, x_max=30, y_min=-30, y_max=30, time_span_s=20)
 
-    obst1_state = State(-5.0, 15.0, 0.0, 1.0)
-    obst1 = Obstacle(obst1_state, yaw_rate_rps=np.deg2rad(10), width_m=1.0)
-    vis.add_object(obst1)
-
-    obst2_state = State(-15.0, -15.0, 0.0, 0.0)
-    obst2 = Obstacle(obst2_state, length_m=10.0, width_m=5.0)
-    vis.add_object(obst2)
-
-    obst3_state = State(20.0, 0.0, 0.0, 0.0)
-    obst3 = Obstacle(obst3_state, yaw_rate_rps=np.deg2rad(15))
-    vis.add_object(obst3)
+    # obstacle instances
+    obst_list = ObstacleList()
+    obst1 = Obstacle(State(-5.0, 15.0, 0.0, 1.0), yaw_rate_rps=np.deg2rad(10), width_m=1.0)
+    obst_list.add_obstacle(obst1)
+    obst2 = Obstacle(State(-15.0, -15.0, 0.0, 0.0), length_m=10.0, width_m=5.0)
+    obst_list.add_obstacle(obst2)
+    obst3 = Obstacle(State(20.0, 0.0, 0.0, 0.0), yaw_rate_rps=np.deg2rad(15))
+    obst_list.add_obstacle(obst3)
+    vis.add_object(obst_list)
 
     # vehicle instance
     spec = VehicleSpecification()
