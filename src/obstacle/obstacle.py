b3be0d8a20e6d89b2ab37bee6f8fcc5d914dca07
Shisato <shisatoyano@gmail.com>
2023-05-05 19:50:15 +0900

    implemented function, update and draw

--- src/obstacle/obstacle.py
+++ src/obstacle/obstacle.py
@@ -13,9 +13,26 @@ from xy_array import XYArray
 
 
 class Obstacle:
-    def __init__(self, state, length_m=2.0, width_m=2.0):
+    def __init__(self, state, accel_mps2=0.0, yaw_rate_rps=0.0,
+                 length_m=2.0, width_m=2.0):
         self.state = state
-        
+        self.accel_mps2 = accel_mps2
+        self.yaw_rate_rps = yaw_rate_rps
+
         contour = np.array([[length_m, -length_m, -length_m, length_m, length_m],
                             [width_m, width_m, -width_m, -width_m, width_m]])
         self.array = XYArray(contour)
+    
+    def update(self, time_s):
+        updated_state = self.state.update(self.accel_mps2, self.yaw_rate_rps, time_s)
+        self.state = updated_state
+    
+    def draw(self, axes, elems):
+        x_m = self.state.get_x_m()
+        y_m = self.state.get_y_m()
+        yaw_rad = self.state.get_yaw_rad()
+
+        transformed_array = self.array.homogeneous_transformation(x_m, y_m, yaw_rad)
+        data = transformed_array.get_data()
+        obstacle_plot, = axes.plot(data[0, :], data[1, :], lw=1.0, color='k', ls='-')
+        elems.append(obstacle_plot)
