a8d3c3a88e3fdfb159129ba3f586c541c3782e53
Shisato <shisatoyano@gmail.com>
2023-05-05 23:17:39 +0900

    implemented constructor, update and draw functions

---
+++ src/obstacle/obstacle_list.py
@@ -0,0 +1,18 @@
+"""
+obstacle_list.py
+
+Author: Shisato Yano
+"""
+
+class ObstacleList:
+    def __init__(self):
+        self.list = []
+    
+    def add_obstacle(self, obstacle):
+        self.list.append(obstacle)
+    
+    def update(self, times_s):
+        for obst in self.list: obst.update(times_s)
+    
+    def draw(self, axes, elems):
+        for obst in self.list: obst.draw(axes, elems)
