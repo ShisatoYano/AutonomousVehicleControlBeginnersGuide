f4596370dc29ccd8166572bca489efbb1323d6e0
Shisato <shisatoyano@gmail.com>
2023-05-08 22:02:41 +0900

    implemented draw function

--- src/sensor/lidar/scan_point.py
+++ src/sensor/lidar/scan_point.py
@@ -17,3 +17,10 @@ class ScanPoint:
         self.distance_m = distance_m
         self.angle_rad = angle_rad
         self.point_array = XYArray(np.array([[x_m], [y_m]]))
+    
+    def draw(self, axes, x_m, y_m, angle_rad, elems):
+        transformed_array = self.point_array.homogeneous_transformation(x_m, y_m, angle_rad)
+        point_plot, = axes.plot(transformed_array.get_x_data(), 
+                                transformed_array.get_y_data(), 
+                                marker='.', color='b')
+        elems.append(point_plot)
