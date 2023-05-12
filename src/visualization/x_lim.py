877e0eb8b7b05bec3d80e4934576c7bc430b8183
Shisato <shisatoyano@gmail.com>
2023-05-12 23:14:17 +0900

    implemented constructor, getter min and max

---
+++ src/visualization/x_lim.py
@@ -0,0 +1,16 @@
+"""
+x_lim.py
+
+Author: Shisato Yano
+"""
+
+class XLim:
+    def __init__(self, min=0, max=10):
+        self.min = min
+        self.max = max
+    
+    def min_value(self):
+        return self.min
+    
+    def max_value(self):
+        return self.max
