915e9e791ea0bf0d58698db6051aa327c0790e20
Shisato <shisatoyano@gmail.com>
2023-05-12 23:20:40 +0900

    added comment about this class

--- src/visualization/x_lim.py
+++ src/visualization/x_lim.py
@@ -5,6 +5,10 @@ Author: Shisato Yano
 """
 
 class XLim:
+    """
+    Limitation values class of x axis
+    """
+
     def __init__(self, min=0, max=10):
         self.min = min
         self.max = max
