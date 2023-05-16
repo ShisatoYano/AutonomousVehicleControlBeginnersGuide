ff8f000c91a7581fda8f3e9263de55985a674e2b
Shisato <shisatoyano@gmail.com>
2023-05-16 23:34:11 +0900

    renamed private functions

--- src/control/pure_pursuit/pure_pursuit_controller.py
+++ src/control/pure_pursuit/pure_pursuit_controller.py
@@ -31,17 +31,17 @@ class PurePursuitController:
         self.target_steer_rad = 0.0
         self.target_yaw_rate_rps = 0.0
     
-    def calculate_look_ahead_distance(self, state):
+    def _calculate_look_ahead_distance(self, state):
         """
-        Function to calculate look ahead distance to target point
+        Private function to calculate look ahead distance to target point
         state: Vehicle's state object
         """
         
         self.look_ahead_distance_m = self.LOOK_FORWARD_GAIN * state.get_speed_mps() + self.MIN_LOOK_AHEAD_DISTANCE_M
 
-    def calculate_target_course_index(self, state):
+    def _calculate_target_course_index(self, state):
         """
-        Function to calculate target point's index on course
+        Private function to calculate target point's index on course
         state: Vehicle's state object
         """
         
@@ -51,27 +51,27 @@ class PurePursuitController:
             nearest_index += 1
         self.target_course_index = nearest_index
 
-    def calculate_target_acceleration_mps2(self, state):
+    def _calculate_target_acceleration_mps2(self, state):
         """
-        Function to calculate acceleration input
+        Private function to calculate acceleration input
         state: Vehicle's state object
         """
 
         diff_speed_mps = self.course.calculate_speed_difference_mps(state, self.target_course_index)
         self.target_accel_mps2 = self.SPEED_PROPORTIONAL_GAIN * diff_speed_mps
 
-    def calculate_target_steer_angle_rad(self, state):
+    def _calculate_target_steer_angle_rad(self, state):
         """
-        Function to calculate steering angle input
+        Private function to calculate steering angle input
         state: Vehicle's state object
         """
         
         diff_angle_rad = self.course.calculate_angle_difference_rad(state, self.target_course_index)
         self.target_steer_rad = atan2((2 * self.WHEEL_BASE_M * sin(diff_angle_rad)), self.look_ahead_distance_m)
 
-    def calculate_target_yaw_rate_rps(self, state):
+    def _calculate_target_yaw_rate_rps(self, state):
         """
-        Function to calculate yaw rate input
+        Private function to calculate yaw rate input
         state: Vehicle's state object
         """
 
@@ -85,15 +85,15 @@ class PurePursuitController:
         
         if not self.course: return
 
-        self.calculate_look_ahead_distance(state)
+        self._calculate_look_ahead_distance(state)
         
-        self.calculate_target_course_index(state)
+        self._calculate_target_course_index(state)
 
-        self.calculate_target_acceleration_mps2(state)
+        self._calculate_target_acceleration_mps2(state)
 
-        self.calculate_target_steer_angle_rad(state)
+        self._calculate_target_steer_angle_rad(state)
 
-        self.calculate_target_yaw_rate_rps(state)
+        self._calculate_target_yaw_rate_rps(state)
     
     def get_target_accel_mps2(self):
         """
