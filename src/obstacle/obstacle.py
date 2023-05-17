b0e288047c16706e79eddd930b94ec16ae7bca6b
Shisato <shisatoyano@gmail.com>
2023-05-17 22:05:46 +0900

    added comments

--- src/obstacle/obstacle.py
+++ src/obstacle/obstacle.py
@@ -13,8 +13,21 @@ from xy_array import XYArray
 
 
 class Obstacle:
+    """
+    Obstacle's data and logic class
+    """
+
     def __init__(self, state, accel_mps2=0.0, yaw_rate_rps=0.0,
                  length_m=2.0, width_m=2.0):
+        """
+        Constructor
+        state: Obstacle's state object
+        accel_mps2: acceleration input to move[m/s2]
+        yaw_rate_rps: yaw rate input to move[rad/s]
+        length_m: Obstacle's longitudinal size[m]
+        width_m: Obstacle's half of lateral size[m]
+        """
+
         self.state = state
         self.accel_mps2 = accel_mps2
         self.yaw_rate_rps = yaw_rate_rps
@@ -24,9 +37,20 @@ class Obstacle:
         self.array = XYArray(contour)
     
     def update(self, time_s):
+        """
+        Function to update obstacle's state
+        time_s: Time interval value[sec]
+        """
+        
         self.state.update(self.accel_mps2, self.yaw_rate_rps, time_s)
     
     def draw(self, axes, elems):
+        """
+        Function to draw obstacle
+        axes: Axes object of figure
+        elems: List of plot objects
+        """
+        
         x_m = self.state.get_x_m()
         y_m = self.state.get_y_m()
         yaw_rad = self.state.get_yaw_rad()
@@ -38,6 +62,10 @@ class Obstacle:
         elems.append(obstacle_plot)
     
     def vertex_xy(self):
+        """
+        Function to get obstacle's vertex point coordinates
+        """
+        
         x_m = self.state.get_x_m()
         y_m = self.state.get_y_m()
         yaw_rad = self.state.get_yaw_rad()
