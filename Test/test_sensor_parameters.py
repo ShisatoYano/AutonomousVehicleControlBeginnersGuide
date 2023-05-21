2373c94a1fcf78409593c224a47a804e597995d8
Shisato <shisatoyano@gmail.com>
2023-05-21 22:01:53 +0900

    implemented test to set arguments

--- Test/test_sensor_parameters.py
+++ Test/test_sensor_parameters.py
@@ -26,3 +26,16 @@ def test_initialize():
     assert param.inst_pos_array != None
     assert param.global_x_m == None
     assert param.global_y_m == None
+
+
+def test_set_arguments():
+    param = SensorParameters(lon_m=1.0, lat_m=2.0, min_m=3.0, max_m=4.0,
+                             reso_deg=5.0, angle_std_scale=6.0, dist_std_rate=7.0)
+    
+    assert param.INST_LON_M == 1.0
+    assert param.INST_LAT_M == 2.0
+    assert param.MIN_RANGE_M == 3.0
+    assert param.MAX_RANGE_M == 4.0
+    assert param.RESO_RAD == np.deg2rad(5.0)
+    assert param.ANGLE_STD_SCALE == 6.0
+    assert param.DIST_STD_RATE == 7.0
