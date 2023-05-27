d4c336e3832083e6fcc9108bc7c25ee12bb270ab
Shisato <shisatoyano@gmail.com>
2023-05-27 22:05:05 +0900

    added comments

--- src/simulation/perception/lidar_obstacle_sensing/lidar_obstacle_sensing.py
+++ src/simulation/perception/lidar_obstacle_sensing/lidar_obstacle_sensing.py
@@ -4,12 +4,14 @@ lidar_obstacle_sensing.py
 Author: Shisato Yano
 """
 
+# import path setting
 import numpy as np
 import sys
 from pathlib import Path
 
 abs_dir_path = str(Path(__file__).absolute().parent)
 relative_path = "/../../../"
+
 sys.path.append(abs_dir_path + relative_path + "visualization")
 sys.path.append(abs_dir_path + relative_path + "state")
 sys.path.append(abs_dir_path + relative_path + "vehicle")
@@ -17,6 +19,8 @@ sys.path.append(abs_dir_path + relative_path + "obstacle")
 sys.path.append(abs_dir_path + relative_path + "sensors")
 sys.path.append(abs_dir_path + relative_path + "sensors/lidar")
 
+
+# import component modules
 from global_xy_visualizer import GlobalXYVisualizer
 from min_max import MinMax
 from time_parameters import TimeParameters
@@ -30,14 +34,21 @@ from sensor_parameters import SensorParameters
 from omni_directional_lidar import OmniDirectionalLidar
 
 
+# flag to show plot figure
+# when executed as unit test, this flag is set as false
 show_plot = True
 
 
 def main():
+    """
+    Main process function
+    """
+    
+    # set simulation parameters
     x_lim, y_lim = MinMax(-30, 30), MinMax(-30, 30)
     vis = GlobalXYVisualizer(x_lim, y_lim, TimeParameters(span_sec=20))
 
-    # obstacle instances
+    # create obstacle instances
     obst_list = ObstacleList()
     obst1 = Obstacle(State(x_m=-5.0, y_m=15.0, speed_mps=1.0), yaw_rate_rps=np.deg2rad(10), width_m=1.0)
     obst_list.add_obstacle(obst1)
@@ -47,17 +58,19 @@ def main():
     obst_list.add_obstacle(obst3)
     vis.add_object(obst_list)
 
-    # vehicle instance
-    spec = VehicleSpecification(area_size=30.0)
-    # sensors
-    lidar = OmniDirectionalLidar(obst_list, SensorParameters(lon_m=spec.wheel_base_m/2))
-    vehicle = FourWheelsVehicle(State(color=spec.color), spec, sensors=Sensors(lidar=lidar))
+    # create vehicle instance
+    spec = VehicleSpecification(area_size=30.0) # spec instance
+    lidar = OmniDirectionalLidar(obst_list, SensorParameters(lon_m=spec.wheel_base_m/2)) # lidar instance
+    vehicle = FourWheelsVehicle(State(color=spec.color), spec, sensors=Sensors(lidar=lidar)) # set state, spec, lidar as arguments
     vis.add_object(vehicle)
 
+    # plot figure is not shown when executed as unit test
     if not show_plot: vis.not_show_plot()
 
+    # show plot figure
     vis.draw()
 
 
+# execute main process
 if __name__ == "__main__":
     main()
