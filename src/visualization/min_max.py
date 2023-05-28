a6ade282affd426bb98299609e95f7eb6b819045
Shisato <shisatoyano@gmail.com>
2023-05-28 10:14:31 +0900

    added comments

--- src/visualization/min_max.py
+++ src/visualization/min_max.py
@@ -10,11 +10,25 @@ class MinMax:
     """
 
     def __init__(self, min=0, max=10):
+        """
+        Constructor
+        min: Minimum value of axis
+        max: Maximum value of axis
+        """
+
         self.min = min
         self.max = max
     
     def min_value(self):
+        """
+        Function to get minimum value
+        """
+        
         return self.min
     
     def max_value(self):
+        """
+        Function to get maximum value
+        """
+        
         return self.max
