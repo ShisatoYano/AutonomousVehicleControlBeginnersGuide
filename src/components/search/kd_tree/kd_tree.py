eb391bd0cc3dee96f9d72b89f517e5d99d364dcb
Shisato Yano <shisatoyano@gmail.com>
2023-07-26 07:04:31 +0000

    implemented neighbor points search

--- src/components/search/kd_tree/kd_tree.py
+++ src/components/search/kd_tree/kd_tree.py
@@ -9,6 +9,7 @@ import numpy as np
 import sys
 from pathlib import Path
 import matplotlib.pyplot as plt
+import matplotlib.patches as ptc
 import time
 
 abs_dir_path = str(Path(__file__).absolute().parent)
@@ -47,7 +48,7 @@ class KdTree:
         """
 
         self.root = self._build_tree(scan_points_list)
-        self.stack = []
+        self.candidates_stack = []
         
     
     def _build_tree(self, scan_points_list, depth=0):
@@ -72,20 +73,30 @@ class KdTree:
         node.right_child = self._build_tree(scan_points_list[median+1:], depth+1)
 
         return node
-    
-    def _push_stack(self, target_point_array, node):
-        """
-        Private function to push node into stack.
-        Pushed nodes are candidates of nearest neighbor point with target
-        """
-        
-        if node:
-            self.stack.append(node)
-            axis = node.axis
-            if target_point_array[axis] < node.data.get_point_array()[axis]:
-                self._push_stack(target_point_array, node.left_child)
+
+    def _search_edge_node(self, target_point_array, node):
+        if not node.left_child and not node.right_child: return node
+
+        axis = node.axis
+        if target_point_array[axis] <= node.data.get_point_array()[axis]:
+            if node.left_child:
+                return self._search_edge_node(target_point_array, node.left_child)
             else:
-                self._push_stack(target_point_array, node.right_child)
+                return node
+        else:
+            if node.right_child:
+                return self._search_edge_node(target_point_array, node.right_child)
+            else:
+                return node
+    
+    def _search_nearest_neighbor_candidates(self, target_point_array, search_radius, node):
+        if node:
+            axis = node.axis
+            diff_axis = abs(target_point_array[axis] - node.data.get_point_array()[axis])
+            if search_radius > diff_axis: 
+                self.candidates_stack.append(node)
+            self._search_nearest_neighbor_candidates(target_point_array, search_radius, node.left_child)
+            self._search_nearest_neighbor_candidates(target_point_array, search_radius, node.right_child)
 
     def search_nearest_neighbor_point(self, target_point):
         """
@@ -94,64 +105,33 @@ class KdTree:
         
         target_point_array = target_point.get_point_array()
 
-        self._push_stack(target_point_array, self.root)
+        edge_node = self._search_edge_node(target_point_array, self.root)
+        search_radius = np.linalg.norm(target_point_array - edge_node.data.get_point_array())
 
-        nearest_node = None
+        self._search_nearest_neighbor_candidates(target_point_array, search_radius, self.root)
+        min_dist = search_radius
+        nearest_neighbor_node = edge_node
 
-        while self.stack:
-            node = self.stack.pop()
-            axis = node.axis
-            dist = np.linalg.norm(target_point_array - node.data.get_point_array())
+        while self.candidates_stack:
+            candidate_node = self.candidates_stack.pop()
+            candidate_dist = np.linalg.norm(target_point_array - candidate_node.data.get_point_array())
+            if min_dist > candidate_dist:
+                min_dist = candidate_dist
+                nearest_neighbor_node = candidate_node
 
-            if not nearest_node:
-                nearest_node = node
-                min_dist = dist
-            else:
-                diff_axis = abs(target_point_array[axis] - node.data.get_point_array()[axis])
-                if diff_axis <= min_dist:
-                    if min_dist > dist:
-                        min_dist = dist
-                        nearest_node = node
-                    
-                    if node.left_child:
-                        dist_left = np.linalg.norm(target_point_array - node.left_child.data.get_point_array())
-                        if min_dist > dist_left:
-                            min_dist = dist_left
-                            nearest_node = node.left_child
-                    
-                    if node.right_child:
-                        dist_right = np.linalg.norm(target_point_array - node.right_child.data.get_point_array())
-                        if min_dist > dist_right:
-                            min_dist = dist_right
-                            nearest_node = node.right_child        
-
-        return nearest_node.data
-    
+        return nearest_neighbor_node.data
 
     def search_neighbor_points_within_r(self, target_point, r=0.2):
         target_point_array = target_point.get_point_array()
 
-        self._push_stack(target_point_array, self.root)
+        self._search_nearest_neighbor_candidates(target_point_array, r, self.root)
 
         neighbor_points = []
-
-        while self.stack:
-            node = self.stack.pop()
-            axis = node.axis
-            dist = np.linalg.norm(target_point_array - node.data.get_point_array())
-
-            if r >= dist:
-                neighbor_points.append(node.data)
-
-                if node.left_child:
-                    dist_left = np.linalg.norm(target_point_array - node.left_child.data.get_point_array())
-                    if r >= dist_left:
-                        neighbor_points.append(node.left_child.data)
-                
-                if node.right_child:
-                    dist_right = np.linalg.norm(target_point_array - node.right_child.data.get_point_array())
-                    if r >= dist_right:
-                        neighbor_points.append(node.right_child.data)
+        while self.candidates_stack:
+            candidate_node = self.candidates_stack.pop()
+            candidate_dist = np.linalg.norm(target_point_array - candidate_node.data.get_point_array())
+            if r > candidate_dist:
+                neighbor_points.append(candidate_node.data)
 
         return neighbor_points
 
@@ -159,37 +139,33 @@ class KdTree:
 def main():
     np.random.seed(20)
     
-    points_xy = np.random.random((10, 2))
+    points_xy = np.random.random((100, 2))
     point_cloud = [ScanPoint(0.0, 0.0, xy[0], xy[1]) for xy in points_xy]
 
-    targets_xy = np.random.random((3, 2))
+    targets_xy = np.random.random((1, 2))
     target_points = [ScanPoint(0.0, 0.0, xy[0], xy[1]) for xy in targets_xy]
 
     for point in point_cloud:
         point_xy = point.get_point_array()
         plt.plot(point_xy[0], point_xy[1], "og")
 
-    # target_point_1 = ScanPoint(0.0, 0.0, 0.035, 0.51)
-    # target_point_2 = ScanPoint(0.0, 0.0, 0.05, 0.9)
-    # target_point_3 = ScanPoint(0.0, 0.0, 0.6, 0.5)
-    # target_point_4 = ScanPoint(0.0, 0.0, 0.8, 0.9)
-
     kd_tree = KdTree(point_cloud)
 
     for target_point in target_points:
         # nearest_point = kd_tree.search_nearest_neighbor_point(target_point)
-        neighbor_points = kd_tree.search_neighbor_points_within_r(target_point)
         # nearest_xy = nearest_point.get_point_array()
+
+        neighbor_points = kd_tree.search_neighbor_points_within_r(target_point)
         
         target_xy = target_point.get_point_array()
-        plt.plot(target_xy[0], target_xy[1], "xb")
+        plt.plot(target_xy[0], target_xy[1], "xb")  
+        # plt.plot([nearest_xy[0], target_xy[0]], [nearest_xy[1], target_xy[1]], "-r")
 
         for neighbor_point in neighbor_points:
-          neighbor_xy = neighbor_point.get_point_array()
-          plt.plot([neighbor_xy[0], target_xy[0]], [neighbor_xy[1], target_xy[1]], "-r")  
-
-        # plt.plot([nearest_xy[0], target_xy[0]], [nearest_xy[1], target_xy[1]], "-r")
+            neighbor_xy = neighbor_point.get_point_array()
+            plt.plot([neighbor_xy[0], target_xy[0]], [neighbor_xy[1], target_xy[1]], "-r")
     
+    plt.axis("square")
     plt.show()
 
 
