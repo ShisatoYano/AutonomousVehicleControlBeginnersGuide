dd300bdda3e72ee3f575def80d7d668cb04673a5
Shisato <shisatoyano@gmail.com>
2023-11-01 23:45:03 +0800

    add state as argument

--- src/components/localization/kalman_filter/extended_kalman_filter_localizer.py
+++ src/components/localization/kalman_filter/extended_kalman_filter_localizer.py
@@ -4,15 +4,26 @@ extended_kalman_filter_localizer.py
 Author: Shisato Yano
 """
 
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).absolute().parent) + "/../../state")
+from state import State
+
 class ExtendedKalmanFilterLocalizer:
     """
     Self localization by Extended Kalman Filter class
     """
 
-    def __init__(self, color='r'):
+    def __init__(self, state, color='r'):
         """
         Constructor
+        state: Vehicle's state object
         color: Color of drawing error covariance ellipse
         """
-
-        self.color = color
+        
+        self.state = state
+        self.DRAW_COLOR = color
+    
+    def update(self, accel_mps2, yaw_rate_rps, time_s, gnss):
+        pass
