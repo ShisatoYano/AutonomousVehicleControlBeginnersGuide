0642ab08754b59da866c199685ed2a67e68d0290
Shisato <shisatoyano@gmail.com>
2023-11-06 23:58:56 +0800

    define input noise variance matrix

--- src/components/localization/kalman_filter/extended_kalman_filter_localizer.py
+++ src/components/localization/kalman_filter/extended_kalman_filter_localizer.py
@@ -5,6 +5,7 @@ Author: Shisato Yano
 """
 
 import sys
+import numpy as np
 from pathlib import Path
 
 sys.path.append(str(Path(__file__).absolute().parent) + "/../../state")
@@ -15,7 +16,8 @@ class ExtendedKalmanFilterLocalizer:
     Self localization by Extended Kalman Filter class
     """
 
-    def __init__(self, state, color='r'):
+    def __init__(self, state, accel_noise_std=1.0, yaw_rate_noise=30.0, 
+                 color='r'):
         """
         Constructor
         state: Vehicle's state object
@@ -23,6 +25,7 @@ class ExtendedKalmanFilterLocalizer:
         """
         
         self.state = state
+        self.INPUT_NOISE_VAR_MAT = np.diag([accel_noise_std, np.deg2rad(yaw_rate_noise)]) ** 2
         self.DRAW_COLOR = color
     
     def update(self, accel_mps2, yaw_rate_rps, time_s, gnss):
