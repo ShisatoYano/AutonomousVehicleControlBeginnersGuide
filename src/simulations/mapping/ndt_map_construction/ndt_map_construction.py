c2bf2aa228e34e0e3a2e76ae620902f736fd84c4
Shisato Yano <shisatoyano@gmail.com>
2023-12-11 05:54:22 +0000

    add source file of ndt map simulation

---
+++ src/simulations/mapping/ndt_map_construction/ndt_map_construction.py
@@ -0,0 +1,72 @@
+"""
+ndt_map_construction.py
+
+Author: Shisato Yano
+"""
+
+# import path setting
+import numpy as np
+import sys
+from pathlib import Path
+
+abs_dir_path = str(Path(__file__).absolute().parent)
+relative_path = "/../../../components/"
+
+sys.path.append(abs_dir_path + relative_path + "visualization")
+sys.path.append(abs_dir_path + relative_path + "state")
+sys.path.append(abs_dir_path + relative_path + "vehicle")
+sys.path.append(abs_dir_path + relative_path + "obstacle")
+sys.path.append(abs_dir_path + relative_path + "sensors")
+sys.path.append(abs_dir_path + relative_path + "sensors/lidar")
+
+
+# import component modules
+from global_xy_visualizer import GlobalXYVisualizer
+from min_max import MinMax
+from time_parameters import TimeParameters
+from vehicle_specification import VehicleSpecification
+from state import State
+from four_wheels_vehicle import FourWheelsVehicle
+from obstacle import Obstacle
+from obstacle_list import ObstacleList
+from sensors import Sensors
+from sensor_parameters import SensorParameters
+from omni_directional_lidar import OmniDirectionalLidar
+
+
+# flag to show plot figure
+# when executed as unit test, this flag is set as false
+show_plot = True
+
+
+def main():
+    """
+    Main process function
+    """
+    
+    # set simulation parameters
+    x_lim, y_lim = MinMax(-30, 30), MinMax(-30, 30)
+    vis = GlobalXYVisualizer(x_lim, y_lim, TimeParameters(span_sec=20))
+
+    # create obstacle instances
+    obst_list = ObstacleList()
+    obst1 = Obstacle(State(x_m=10.0, y_m=10.0))
+    obst_list.add_obstacle(obst1)
+    vis.add_object(obst_list)
+
+    # create vehicle instance
+    spec = VehicleSpecification() # spec instance
+    lidar = OmniDirectionalLidar(obst_list, SensorParameters(lon_m=spec.wheel_base_m/2, angle_std_scale=0.0, dist_std_rate=0.0)) # lidar instance
+    vehicle = FourWheelsVehicle(State(color=spec.color), spec, sensors=Sensors(lidar=lidar), show_zoom=False) # set state, spec, lidar as arguments
+    vis.add_object(vehicle)
+
+    # plot figure is not shown when executed as unit test
+    if not show_plot: vis.not_show_plot()
+
+    # show plot figure
+    vis.draw()
+
+
+# execute main process
+if __name__ == "__main__":
+    main()
