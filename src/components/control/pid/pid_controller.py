e133c7e8caf687a605e02807032f52de1222286a
ShisatoYano <shisatoyano@gmail.com>
2024-06-24 14:35:28 +0000

    defined constructor

--- src/components/control/pid/pid_controller.py
+++ src/components/control/pid/pid_controller.py
@@ -3,3 +3,18 @@ pid_controller.py
 
 Author: Shisato Yano
 """
+
+
+class PidController:
+    """
+    Controller class by PID control algorithm
+    """
+
+    def __init__(self, spec, course=None):
+        """
+        Constructor
+        spec: Vehicle specification object
+        course: Course data and logic object
+        """
+
+        self.WHEEL_BASE_M = spec.wheel_base_m
