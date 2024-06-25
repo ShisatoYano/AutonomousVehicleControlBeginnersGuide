50c173db067309225a83b1a08cdfce8ccbda2c8e
ShisatoYano <shisatoyano@gmail.com>
2024-06-25 14:40:22 +0000

    implemented look ahead distance calculation

--- src/components/control/pid/pid_controller.py
+++ src/components/control/pid/pid_controller.py
@@ -31,6 +31,14 @@ class PidController:
         self.target_steer_rad = 0.0
         self.target_yaw_rate_rps = 0.0
     
+    def _calculate_look_ahead_distance(self, state):
+        """
+        Private function to calculate look ahead distance to target point
+        state: Vehicle's state object
+        """
+        
+        self.look_ahead_distance_m = self.LOOK_FORWARD_GAIN * state.get_speed_mps() + self.MIN_LOOK_AHEAD_DISTANCE_M
+
     def update(self, state):
         """
         Function to update data for path tracking
@@ -38,3 +46,5 @@ class PidController:
         """
 
         if not self.course: return
+
+        self._calculate_look_ahead_distance(state)
