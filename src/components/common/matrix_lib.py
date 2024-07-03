ca41def5a0316c477863f9193d5bd462e2f20f4d
ShisatoYano <shisatoyano@gmail.com>
2024-07-03 14:37:38 +0000

    calculate lon-lat error against course

--- src/components/common/matrix_lib.py
+++ src/components/common/matrix_lib.py
@@ -21,3 +21,10 @@ def hom_mat_33(x, y, yaw):
     return np.array([[cos_yaw, -sin_yaw, x],
                      [sin_yaw, cos_yaw, y],
                      [0.0, 0.0, 1.0]])
+
+
+def rot_mat_22(yaw):
+    cos_yaw, sin_yaw = cos(yaw), sin(yaw)
+
+    return np.array([[cos_yaw, -sin_yaw],
+                     [sin_yaw, cos_yaw]])
