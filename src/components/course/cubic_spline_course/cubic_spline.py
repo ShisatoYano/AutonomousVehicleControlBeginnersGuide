c1612e97d427f40b3814eec2cf337238b9ef42e1
ShisatoYano <shisatoyano@gmail.com>
2024-07-08 15:22:05 +0000

    add comments about class and constructor

--- src/components/course/cubic_spline_course/cubic_spline.py
+++ src/components/course/cubic_spline_course/cubic_spline.py
@@ -10,9 +10,17 @@ import numpy as np
 class CubicSpline:
     """
     1D Cubic Spline class
+    Each section between two points are approximated as the following equation
+    y_i = a_i + b_i * (x - x_i) + c_i * (x - x_i)^2 + d_i * (x - x_i)^3
     """
 
     def __init__(self, x_points, y_points):
+        """
+        Constructor
+        x_points: List of x coordinate points. This must be stored in ascending order.
+        y_points: List of y coordinate points.
+        """
+        
         h = np.diff(x_points)
         if np.any(h < 0):
             raise ValueError("X coordinate points must be stored in ascending order")
