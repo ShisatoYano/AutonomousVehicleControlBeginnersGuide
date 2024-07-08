78d7e9440ed18860f0c461875ef0333d7938ed12
ShisatoYano <shisatoyano@gmail.com>
2024-07-08 15:11:12 +0000

    add value error exception for x points input

--- src/components/course/cubic_spline_course/cubic_spline.py
+++ src/components/course/cubic_spline_course/cubic_spline.py
@@ -3,3 +3,16 @@ cubic_spline.py
 
 Author: Shisato Yano
 """
+
+import numpy as np
+
+
+class CubicSpline:
+    """
+    1D Cubic Spline class
+    """
+
+    def __init__(self, x_points, y_points):
+        h = np.diff(x_points)
+        if np.any(h < 0):
+            raise ValueError("X coordinate points must be stored in ascending order")
