2e6ee2fb2bbbc9de878b8c352f61fb1bbfbc2310
ShisatoYano <shisatoyano@gmail.com>
2024-07-09 15:19:01 +0000

    add main function to debug

--- src/simulations/course/cubic_spline/cubic_spline_plot.py
+++ src/simulations/course/cubic_spline/cubic_spline_plot.py
@@ -13,7 +13,7 @@ from pathlib import Path
 abs_dir_path = str(Path(__file__).absolute().parent)
 relative_path = "/../../../components/"
 
-sys.path.append(abs_dir_path + relative_path + "course/cubic_spline")
+sys.path.append(abs_dir_path + relative_path + "course/cubic_spline_course")
 
 # import component module
 from cubic_spline import CubicSpline
@@ -22,3 +22,18 @@ from cubic_spline import CubicSpline
 # flag to show plot figure
 # when executed as unit test, this flag is set as false
 show_plot = True
+
+
+def main():
+    """
+    Main process function
+    """
+
+    x_points = np.arange(5)
+    y_points = [1.7, -6, 5, 6.5, 0.0]
+
+    cubic_spline = CubicSpline(x_points, y_points)
+
+
+if __name__ == "__main__":
+    main()
