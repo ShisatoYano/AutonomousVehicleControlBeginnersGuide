764dfe73eb79db4ffbab9af62925aee9be83d560
ShisatoYano <shisatoyano@gmail.com>
2024-08-19 15:26:10 +0000

    defined class and constructor

---
+++ src/components/course/cubic_spline_course/cubic_spline_2d.py
@@ -0,0 +1,21 @@
+"""
+cubic_spline_2d.py
+
+Author: Shisato Yano
+"""
+
+import numpy as np
+
+from cubic_spline import CubicSpline
+
+
+class CubicSpline2D:
+    """
+    2D Cubic Spline class
+    Each section between two points are approximated as the following equation
+    y_i = a_i + b_i * (x - x_i) + c_i * (x - x_i)^2 + d_i * (x - x_i)^3
+    """
+
+    def __init__(self, x_points, y_points):
+        print(x_points)
+        print(y_points)
