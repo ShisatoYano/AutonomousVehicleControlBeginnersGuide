92ccd235d997f950f4080ff7cf0de8e1a5a1ef72
ShisatoYano <shisatoyano@gmail.com>
2024-12-04 15:10:49 +0000

    import cubic spline 2d class for course creation

--- src/components/course/cubic_spline_course/cubic_spline_course.py
+++ src/components/course/cubic_spline_course/cubic_spline_course.py
@@ -3,3 +3,17 @@ cubic_spline_course.py
 
 Author: Shisato Yano
 """
+
+# import path setting
+import sys
+from pathlib import Path
+
+abs_dir_path = str(Path(__file__).absolute().parent)
+relative_path = "/../../../components/"
+
+sys.path.append(abs_dir_path + relative_path + "course/cubic_spline_course")
+
+#import component modules
+from cubic_spline_2d import CubicSpline2D
+
+print(CubicSpline2D)
