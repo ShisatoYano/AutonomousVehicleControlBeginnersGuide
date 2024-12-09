ada241724004ce136bfe4f30c8a6430de9123355
ShisatoYano <shisatoyano@gmail.com>
2024-12-09 15:02:05 +0000

    declared base points array variable

--- src/components/course/cubic_spline_course/cubic_spline_course.py
+++ src/components/course/cubic_spline_course/cubic_spline_course.py
@@ -7,6 +7,8 @@ Author: Shisato Yano
 # import path setting
 import sys
 from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
 
 abs_dir_path = str(Path(__file__).absolute().parent)
 relative_path = "/../../../components/"
@@ -22,13 +24,16 @@ class CubicSplineCourse:
     Course generation class by Cubic spline interpolation
     """
 
-    def __init__(self, x_ref_points, y_ref_points, target_speed_kmph, color='r'):
+    def __init__(self, x_ref_points, y_ref_points, target_speed_kmph, resolution=0.1, color='r'):
         """
         Constructor
         x_ref_points: Array of reference points in x axis
         y_ref_points: Array of reference points in y axis
         target_speed_kmph: Target speed[km/h] driving on generated course
+        resolution: Distance between 2 interpolated points
         color: Color of drawing course points
         """
         
-        pass
+        cubic_spline = CubicSpline2D(x_ref_points, y_ref_points)
+        base_points = np.arange(0, cubic_spline.s[-1], resolution)
+
