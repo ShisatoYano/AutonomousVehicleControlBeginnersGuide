42de03fe161dcb6d0e154700962bdeffbbb334f3
ShisatoYano <shisatoyano@gmail.com>
2024-12-09 14:47:22 +0000

    defined a constructor and wrote comment

--- src/components/course/cubic_spline_course/cubic_spline_course.py
+++ src/components/course/cubic_spline_course/cubic_spline_course.py
@@ -16,4 +16,19 @@ sys.path.append(abs_dir_path + relative_path + "course/cubic_spline_course")
 #import component modules
 from cubic_spline_2d import CubicSpline2D
 
-print(CubicSpline2D)
+
+class CubicSplineCourse:
+    """
+    Course generation class by Cubic spline interpolation
+    """
+
+    def __init__(self, x_ref_points, y_ref_points, target_speed_kmph, color='r'):
+        """
+        Constructor
+        x_ref_points: Array of reference points in x axis
+        y_ref_points: Array of reference points in y axis
+        target_speed_kmph: Target speed[km/h] driving on generated course
+        color: Color of drawing course points
+        """
+        
+        pass
