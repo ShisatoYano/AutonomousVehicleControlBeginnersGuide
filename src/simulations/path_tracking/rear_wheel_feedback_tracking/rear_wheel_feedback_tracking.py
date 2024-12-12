a640a1f8159b5abbf4c74d5f15497952b6151db1
ShisatoYano <shisatoyano@gmail.com>
2024-12-12 15:26:17 +0000

    setting import path for each modules

--- src/simulations/path_tracking/rear_wheel_feedback_tracking/rear_wheel_feedback_tracking.py
+++ src/simulations/path_tracking/rear_wheel_feedback_tracking/rear_wheel_feedback_tracking.py
@@ -3,3 +3,16 @@ rear_wheel_feedback_tracking.py
 
 Author: Shisato Yano
 """
+
+# import path setting
+import sys
+from pathlib import Path
+
+abs_dir_path = str(Path(__file__).absolute().parent)
+relative_path = "/../../../components/"
+
+sys.path.append(abs_dir_path + relative_path + "visualization")
+sys.path.append(abs_dir_path + relative_path + "state")
+sys.path.append(abs_dir_path + relative_path + "vehicle")
+sys.path.append(abs_dir_path + relative_path + "course/cubic_spline_course")
+sys.path.append(abs_dir_path + relative_path + "control/rear_wheel_feedback")
