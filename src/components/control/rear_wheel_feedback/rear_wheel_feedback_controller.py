307bfeb03db04e06b163da01494df9164c11e575
ShisatoYano <shisatoyano@gmail.com>
2024-12-13 14:55:32 +0000

    defined rear wheel feedback controller class and the constructor

--- src/components/control/rear_wheel_feedback/rear_wheel_feedback_controller.py
+++ src/components/control/rear_wheel_feedback/rear_wheel_feedback_controller.py
@@ -3,3 +3,17 @@ rear_wheel_feedback_controller.py
 
 Author: Shisato Yano
 """
+
+
+class RearWheelFeedbackController:
+    """
+    Controller class by Rear wheel feedback algorithm
+    """
+
+    def __init__(self, course=None):
+        """
+        Constructor
+        course: Course data and logic object
+        """
+
+        self.course = course
