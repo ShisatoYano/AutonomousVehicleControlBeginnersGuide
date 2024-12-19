8f7f15589b7e87d0e3861792a3296bcee24425f5
ShisatoYano <shisatoyano@gmail.com>
2024-12-19 14:50:58 +0000

    add test case of lower than pi

--- test/test_angle_lib.py
+++ test/test_angle_lib.py
@@ -11,3 +11,9 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).absolute().parent) + "/../src/components/common")
 from angle_lib import pi_to_pi
+
+
+def test_lower_than_pi():
+    assert pi_to_pi(0.0) == 0.0
+    assert pi_to_pi(np.pi / 2) == np.pi / 2
+    assert pi_to_pi(np.pi) == -np.pi
