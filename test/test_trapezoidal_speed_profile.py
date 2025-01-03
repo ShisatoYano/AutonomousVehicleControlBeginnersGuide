e4c390d7776ebcd4fe1ebd3a34998f6e6497320a
ShisatoYano <shisatoyano@gmail.com>
2025-01-03 14:51:55 +0000

    assert max spd[m/s]

--- test/test_trapezoidal_speed_profile.py
+++ test/test_trapezoidal_speed_profile.py
@@ -10,3 +10,11 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../src/components/control/speed_profile")
 from trapezoidal_speed_profile import TrapezoidalSpeedProfile
+
+
+# test instance
+spd_prf = TrapezoidalSpeedProfile(10, 2, 100)
+
+
+def test_init():
+    assert spd_prf.max_spd_mps == 10
