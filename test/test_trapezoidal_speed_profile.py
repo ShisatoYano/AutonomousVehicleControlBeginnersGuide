e7397cef3b69296955555f1be6f4594df162dc23
ShisatoYano <shisatoyano@gmail.com>
2025-01-06 14:03:23 +0000

    passed test case of initialization

--- test/test_trapezoidal_speed_profile.py
+++ test/test_trapezoidal_speed_profile.py
@@ -18,3 +18,11 @@ spd_prf = TrapezoidalSpeedProfile(10, 2, 100)
 
 def test_init():
     assert spd_prf.max_spd_mps == 10
+    assert spd_prf.max_accel_mps2 == 2
+    assert spd_prf.distance_m == 100
+
+    assert spd_prf.accel_time_s == 5
+    assert spd_prf.decel_time_s == 5
+    assert spd_prf.const_time_s == 5
+
+    assert spd_prf.target_speed_mps == 0.0
