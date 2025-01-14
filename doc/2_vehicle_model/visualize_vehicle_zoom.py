7271c7658dbfbe6a2709eac97f3d42e0f83cbe96
ShisatoYano <shisatoyano@gmail.com>
2025-01-14 14:42:24 +0000

    add sample code for visualizing vehicle with zoom

---
+++ doc/2_vehicle_model/visualize_vehicle_zoom.py
@@ -0,0 +1,59 @@
+"""
+visualize_world.py
+
+Author: Shisato Yano
+"""
+
+# import path setting
+import numpy as np
+import sys
+from pathlib import Path
+
+abs_dir_path = str(Path(__file__).absolute().parent)
+relative_path = "/../../src/components/"
+
+sys.path.append(abs_dir_path + relative_path + "visualization")
+sys.path.append(abs_dir_path + relative_path + "state")
+sys.path.append(abs_dir_path + relative_path + "vehicle")
+
+
+# import component modules
+from global_xy_visualizer import GlobalXYVisualizer
+from min_max import MinMax
+from time_parameters import TimeParameters
+from vehicle_specification import VehicleSpecification
+from state import State
+from four_wheels_vehicle import FourWheelsVehicle
+
+
+# flag to show plot figure
+# when executed as unit test, this flag is set as false
+show_plot = True
+
+
+def main():
+    """
+    Main process function
+    """
+    
+    # set simulation parameters
+    x_lim, y_lim = MinMax(-30, 30), MinMax(-30, 30)
+    vis = GlobalXYVisualizer(x_lim, y_lim, TimeParameters(span_sec=20))
+
+    # create vehicle
+    spec = VehicleSpecification() # specification
+    vehicle = FourWheelsVehicle(State(), spec) # set state and spec
+
+    # add objects here
+    vis.add_object(vehicle)
+
+    # plot figure is not shown when executed as unit test
+    if not show_plot: vis.not_show_plot()
+
+    # show plot figure
+    vis.draw()
+
+
+# execute main process
+if __name__ == "__main__":
+    main()
