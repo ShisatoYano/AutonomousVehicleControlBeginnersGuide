f71ae3336f9f21ec5c55b9ff318bd40dd74d384b
ShisatoYano <shisatoyano@gmail.com>
2025-01-15 15:17:06 +0000

    defined member variables

--- src/components/control/lqr/lqr_controller.py
+++ src/components/control/lqr/lqr_controller.py
@@ -33,3 +33,11 @@ class LqrController:
         self.SPEED_PROPORTIONAL_GAIN = 1.0
         self.WHEEL_BASE_M = spec.wheel_base_m
         self.MAX_ACCEL_MPS2 = spec.max_accel_mps2
+
+        self.course = course
+        self.target_course_index = 0
+        self.target_accel_mps2 = 0.0
+        self.target_speed_mps = 0.0
+        self.target_yaw_rate_rps = 0.0
+        self.target_steer_rad = 0.0
+        self.elapsed_time_sec = 0.0
