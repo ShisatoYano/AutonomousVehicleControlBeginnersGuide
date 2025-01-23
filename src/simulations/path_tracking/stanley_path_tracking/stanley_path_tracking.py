232076ec0eb445cbd1d65a97dbc779a9ff4ca3e6
ShisatoYano <shisatoyano@gmail.com>
2025-01-23 13:14:14 +0000

    import modules

--- src/simulations/path_tracking/stanley_path_tracking/stanley_path_tracking.py
+++ src/simulations/path_tracking/stanley_path_tracking/stanley_path_tracking.py
@@ -16,3 +16,13 @@ sys.path.append(abs_dir_path + relative_path + "state")
 sys.path.append(abs_dir_path + relative_path + "vehicle")
 sys.path.append(abs_dir_path + relative_path + "course/cubic_spline_course")
 sys.path.append(abs_dir_path + relative_path + "control/stanley")
+
+# import component modules
+from global_xy_visualizer import GlobalXYVisualizer
+from min_max import MinMax
+from time_parameters import TimeParameters
+from vehicle_specification import VehicleSpecification
+from state import State
+from four_wheels_vehicle import FourWheelsVehicle
+from cubic_spline_course import CubicSplineCourse
+from stanley_controller import StanleyController
