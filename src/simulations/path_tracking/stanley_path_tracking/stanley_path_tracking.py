a6c0cdf9032a19435d4e817941cb2b3c010aa46c
ShisatoYano <shisatoyano@gmail.com>
2025-01-23 13:16:25 +0000

    defined flag to switch showing plot for unit test

--- src/simulations/path_tracking/stanley_path_tracking/stanley_path_tracking.py
+++ src/simulations/path_tracking/stanley_path_tracking/stanley_path_tracking.py
@@ -26,3 +26,8 @@ from state import State
 from four_wheels_vehicle import FourWheelsVehicle
 from cubic_spline_course import CubicSplineCourse
 from stanley_controller import StanleyController
+
+
+# flag to show plot figure
+# when executed as unit test, this flag is set as false
+show_plot = True
