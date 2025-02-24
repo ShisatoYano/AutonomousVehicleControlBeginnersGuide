5ed7b9ffdb96ab1709cbec4266ce6cce529256cc
ShisatoYano <shisatoyano@gmail.com>
2025-02-24 15:12:28 +0000

    import path setting

--- src/simulations/mapping/binary_grid_map_construction/binary_grid_map_construction.py
+++ src/simulations/mapping/binary_grid_map_construction/binary_grid_map_construction.py
@@ -3,3 +3,11 @@ binary_grid_map_construction.py
 
 Author: Shisato Yano
 """
+
+# import path setting
+import sys
+import numpy as np
+from pathlib import Path
+
+abs_dir_path = str(Path(__file__).absolute().parent)
+relative_path = "/../../../components/"
