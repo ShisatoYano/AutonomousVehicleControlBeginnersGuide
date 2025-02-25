6dc92e4a99357de6c319d022f07f2ae48ef98500
ShisatoYano <shisatoyano@gmail.com>
2025-02-25 13:45:35 +0000

    append paths to each modules into sys path

--- src/simulations/mapping/binary_grid_map_construction/binary_grid_map_construction.py
+++ src/simulations/mapping/binary_grid_map_construction/binary_grid_map_construction.py
@@ -11,3 +11,14 @@ from pathlib import Path
 
 abs_dir_path = str(Path(__file__).absolute().parent)
 relative_path = "/../../../components/"
+
+sys.path.append(abs_dir_path + relative_path + "visualization")
+sys.path.append(abs_dir_path + relative_path + "state")
+sys.path.append(abs_dir_path + relative_path + "vehicle")
+sys.path.append(abs_dir_path + relative_path + "obstacle")
+sys.path.append(abs_dir_path + relative_path + "sensors")
+sys.path.append(abs_dir_path + relative_path + "sensors/lidar")
+# mapping modules
+sys.path.append(abs_dir_path + relative_path + "course/cubic_spline_course")
+sys.path.append(abs_dir_path + relative_path + "control/pure_pursuit")
+
