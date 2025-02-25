a040551ecef59864bc9085de40936c3c3146c082
ShisatoYano <shisatoyano@gmail.com>
2025-02-25 13:49:42 +0000

    import component modules

--- src/simulations/mapping/binary_grid_map_construction/binary_grid_map_construction.py
+++ src/simulations/mapping/binary_grid_map_construction/binary_grid_map_construction.py
@@ -22,3 +22,19 @@ sys.path.append(abs_dir_path + relative_path + "sensors/lidar")
 sys.path.append(abs_dir_path + relative_path + "course/cubic_spline_course")
 sys.path.append(abs_dir_path + relative_path + "control/pure_pursuit")
 
+
+# import component modules
+from global_xy_visualizer import GlobalXYVisualizer
+from min_max import MinMax
+from time_parameters import TimeParameters
+from vehicle_specification import VehicleSpecification
+from state import State
+from four_wheels_vehicle import FourWheelsVehicle
+from obstacle import Obstacle
+from obstacle_list import ObstacleList
+from sensors import Sensors
+from sensor_parameters import SensorParameters
+from omni_directional_lidar import OmniDirectionalLidar
+# import binary grid mapper module
+from cubic_spline_course import CubicSplineCourse
+from pure_pursuit_controller import PurePursuitController
