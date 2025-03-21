3d6e62749d08afa08c95927821f519d4ebb1c0af
ShisatoYano <shisatoyano@gmail.com>
2025-03-21 14:08:10 +0000

    defined constructor of binary map class

--- src/components/mapping/binary/binary_map.py
+++ src/components/mapping/binary/binary_map.py
@@ -3,3 +3,12 @@ binary_map.py
 
 Author: Shisato Yano
 """
+
+class BinaryMap:
+    """
+    Binary grid map class
+    """
+
+    def __init__(self, width_m=60.0, height_m=60.0, resolution_m=1.0,
+                 center_x_m=0.0, center_y_m=0.0):
+        pass
