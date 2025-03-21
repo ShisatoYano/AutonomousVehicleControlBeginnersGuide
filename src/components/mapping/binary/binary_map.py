b7305292ee46e824e5f5ffa5db26c200131df6b0
ShisatoYano <shisatoyano@gmail.com>
2025-03-21 14:09:34 +0000

    add comment of constructor

--- src/components/mapping/binary/binary_map.py
+++ src/components/mapping/binary/binary_map.py
@@ -11,4 +11,13 @@ class BinaryMap:
 
     def __init__(self, width_m=60.0, height_m=60.0, resolution_m=1.0,
                  center_x_m=0.0, center_y_m=0.0):
+        """
+        Constructor
+        width_m: Width size of map[m]
+        height_m: Height size of map[m]
+        resolution_m: Size of each cells[m]
+        center_x_m: Center x position of map[m]
+        center_y_m: Center y position of map[m]
+        """
+        
         pass
