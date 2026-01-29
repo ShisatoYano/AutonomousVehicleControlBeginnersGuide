d5b482ae45022a6c87d06a67d89d51c78a99eb29
Yuvraj Gupta <guptuv2003@gmail.com>
2026-01-29 15:55:17 +0530

    Added unit test for RRT* path planning

---
+++ test/test_rrt_star_path_planning.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import sys
+import pytest
+
+sys.path.append(
+    str(Path(__file__).absolute().parent)
+    + "/../src/simulations/path_planning/rrt_star_path_planning"
+)
+
+import rrt_star_path_planning
+
+
+def test_simulation():
+    # Disable visualization for CI / testing
+    rrt_star_path_planning.show_plot = False
+
+    # Run simulation (smoke test)
+    rrt_star_path_planning.main()
